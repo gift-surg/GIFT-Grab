042e9d47187da086aec33d310b59530c2e649293
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2015-12-11 11:45:08 +0000

    documented VideoSourceOpenCV

--- src/offline/opencv_video_source.h
+++ src/offline/opencv_video_source.h
@@ -8,14 +8,21 @@
 // OpenCV includes
 #include "opencv2/opencv.hpp"
 
-/**
- * OpenCV video source
- */
-
+//!
+//! \brief OpenCV video source
+//!
+//! Can be used for reading files as well as streaming
+//! from devices
+//!
 class VideoSourceOpenCV: public IVideoSource
 {
 public:
+    //!
+    //! \brief Read in video file specified by \c path
+    //! \param path
+    //!
     VideoSourceOpenCV(const char *path);
+
     //!
     //! \brief Connects to device with specified \c deviceId,
     //! e.g. for streaming
@@ -25,9 +32,13 @@ public:
     //! with \c deviceId
     //!
     VideoSourceOpenCV(const int deviceId);
+
     bool get_frame_dimensions(int & width, int & height);
+
     bool get_frame(VideoFrame_BGRA &frame);
+
     double get_frame_rate();
+
     void set_sub_frame(int x, int y, int width, int height)
     {
         _sub_frame = cv::Rect(x, y, width, height);
@@ -36,10 +47,31 @@ public:
     }
 
 private:
+    //!
+    //! \brief OpenCV capture object
+    //!
     cv::VideoCapture        _cap;
+
+    //!
+    //! \brief Buffer for raw frames
+    //!
     cv::Mat                 _buffer;
+
+    //!
+    //! \brief Buffer for BGRA conversion
+    //!
     cv::Mat                 _buffer_bgra;
+
+    //!
+    //! \brief Buffer for frame ROI, in case
+    //! specified
+    //! \sa _sub_frame
+    //!
     cv::Mat                 _buffer_sub_bgra;
+
+    //!
+    //! \brief Frame ROI
+    //!
     cv::Rect                _sub_frame;
 
     DISALLOW_COPY_AND_ASSIGNMENT(VideoSourceOpenCV);
