373c8d6ed585c50a519e94c46d9ab5686a4beaab
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-04-12 16:15:18 +0100

    Issue #21: test app now accepting codec and port as parameters

--- src/tests/save_1min_epiphan_dvi_xvid.cpp
+++ src/tests/save_1min_epiphan_dvi_xvid.cpp
@@ -2,9 +2,42 @@
 #include <chrono>
 #include <thread>
 
-int main()
+int main(int argc, char ** argv)
 {
-    IVideoSource * epiphan = gg::Factory::connect(gg::Device::DVI2PCIeDuo_DVI);
+    enum gg::Device port = gg::Device::DVI2PCIeDuo_DVI;
+    enum gg::Target codec = gg::Target::File_XviD;
+    std::string port_string = "dvi";
+    std::string codec_string = "xvid";
+
+    if (argc >= 2)
+    {
+        codec_string = std::string(argv[1]);
+        if (codec_string == "xvid")
+            codec = gg::Target::File_XviD;
+        else if (codec_string == "h265")
+            codec = gg::Target::File_H265;
+        else
+        {
+            std::cerr << "Codec " << codec_string << " not recognised" << std::endl;
+            exit(-1);
+        }
+
+        if (argc >= 3)
+        {
+            port_string = std::string(argv[2]);
+            if (port_string == "sdi")
+                port = gg::Device::DVI2PCIeDuo_SDI;
+            else if (port_string == "dvi")
+                port = gg::Device::DVI2PCIeDuo_DVI;
+            else
+            {
+                std::cerr << "Port " << port_string << " not recognised" << std::endl;
+                exit(-1);
+            }
+        }
+    }
+
+    IVideoSource * epiphan = gg::Factory::connect(port);
     epiphan->set_sub_frame(600, 190, 678, 678); // optimal for Storz 27020 AA straight
     float fps = 20;
     int duration = 1; // min
@@ -14,8 +47,15 @@ int main()
 
     try
     {
-        gg::IVideoTarget * file = gg::Factory::create(gg::Target::File_XviD);
-        file->init("2min_xvid_epiphan.avi", fps);
+        gg::IVideoTarget * file = gg::Factory::create(codec);
+        std::string filename;
+        filename.append("1min_")
+                .append(codec_string)
+                .append("_epiphan_")
+                .append(port_string)
+                .append(".avi");
+        std::cout << "Saving to file " << filename << std::endl;
+        file->init(filename, fps);
         for (int i = 0; i < num_frames; i++)
         {
             if (i % 5 == 0)
