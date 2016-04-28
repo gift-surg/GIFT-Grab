bda0eb7b2b6e054d6fd914ce240d3955ca3d406a
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-04-28 15:36:39 +0100

    Issue #26: proper exposition of IVideoSource class to allow for polymorphism

--- src/python/wrapper.cpp
+++ src/python/wrapper.cpp
@@ -1,14 +1,38 @@
 #include "factory.h"
 #include <boost/python.hpp>
 
+using namespace boost::python;
+
 void foo()
 {
     std::cout << "This should print with a dot at the end." << std::endl;
 }
 
+class IVideoSourceWrap : IVideoSource, wrapper<IVideoSource>
+{
+    bool get_frame_dimensions(int & width, int & height)
+    {
+        return this->get_override("get_frame_dimensions")(width, height);
+    }
+
+    bool get_frame(VideoFrame_BGRA & frame)
+    {
+        return this->get_override("get_frame")(frame);
+    }
+
+    double get_frame_rate()
+    {
+        return this->get_override("get_frame_rate")();
+    }
+
+    void set_sub_frame(int x, int y, int width, int height)
+    {
+        this->get_override("set_sub_frame")(x, y, width, height);
+    }
+};
+
 BOOST_PYTHON_MODULE(pygiftgrab)
 {
-    using namespace boost::python;
     def( "foo", foo );
 
     enum_<gg::Device>("gg_Device")
@@ -17,5 +41,9 @@ BOOST_PYTHON_MODULE(pygiftgrab)
     ;
 
     class_<IVideoSource, boost::noncopyable>("IVideoSource", no_init)
+        .def("get_frame_dimensions", pure_virtual(&IVideoSource::get_frame_dimensions))
+        .def("get_frame", pure_virtual(&IVideoSource::get_frame))
+        .def("get_frame_rate", pure_virtual(&IVideoSource::get_frame_rate))
+        .def("set_sub_frame", pure_virtual(&IVideoSource::set_sub_frame))
     ;
 }
