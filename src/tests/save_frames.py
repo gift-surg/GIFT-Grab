5a7e1a4db92f00c8cc881bb90b611a42b5ec28d7
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-03 17:55:33 +0100

    Issue #25: python script reporting total time for recording, and running for 1 min at full resolution now

--- src/tests/save_frames.py
+++ src/tests/save_frames.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 
 import pygiftgrab
+import time
+from datetime import timedelta
 
-frame_rate = 20
-recording_duration = 0.1  # min
+frame_rate = 60
+recording_duration = 1  # min
 num_frames = int(recording_duration * 60 * frame_rate)
 device_type = pygiftgrab.Device.DVI2PCIeDuo_SDI
 storage_type = pygiftgrab.Storage.File_H265
@@ -28,9 +30,10 @@ except (RuntimeError, IOError) as e:
 frame = pygiftgrab.VideoFrame_BGRA(False)  # to avoid "thin wrappers" required for default args
 try:
     source = pygiftgrab.Factory.connect(device_type)
-    source.set_sub_frame(660, 160, 678, 688)
+    # source.set_sub_frame(660, 160, 678, 688)
     target = pygiftgrab.Factory.writer(storage_type)
     target.init(file_path, frame_rate)
+    start = time.time()
     for i in range(1, num_frames+1):
         source.get_frame(frame)
         print 'Frame ' + str(i) + '/' + str(num_frames) + \
@@ -38,6 +41,8 @@ try:
         target.append(frame)
         # TODO
         # sleep(inter_frame_msec)
+    elapsed = (time.time() - start)
+    print "Total time for recording " + str(recording_duration) + " min. was " + str(timedelta(seconds=elapsed))
     target.finalise()
     pygiftgrab.Factory.disconnect(pygiftgrab.Device.DVI2PCIeDuo_SDI)
 except (RuntimeError, IOError) as e:
