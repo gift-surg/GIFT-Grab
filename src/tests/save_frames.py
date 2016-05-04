e988e6e1b7ae7eeb030debe0c4f8a23970d465ee
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-04 08:39:00 +0100

    Issue #25: renamed variables to reflect port type, due to imminent introduction of dual-port recording in python test script

--- src/tests/save_frames.py
+++ src/tests/save_frames.py
@@ -13,29 +13,29 @@ if storage_type == pygiftgrab.Storage.File_H265:
     extension = '.mp4'
 elif storage_type == pygiftgrab.Storage.File_XviD:
     extension = '.avi'
-file_path = str(recording_duration) + '-min-python-recording-from-' + str(device_type) + extension
+file_path_sdi = str(recording_duration) + '-min-python-recording-from-' + str(device_type) + extension
 
-frame = pygiftgrab.VideoFrame_BGRA(False)  # to avoid "thin wrappers" required for default args
+frame_sdi = pygiftgrab.VideoFrame_BGRA(False)  # to avoid "thin wrappers" required for default args
 try:
-    source = pygiftgrab.Factory.connect(device_type)
+    source_sdi = pygiftgrab.Factory.connect(device_type)
     # source.set_sub_frame(660, 160, 678, 688)
-    target = pygiftgrab.Factory.writer(storage_type)
-    target.init(file_path, frame_rate)
+    target_sdi = pygiftgrab.Factory.writer(storage_type)
+    target_sdi.init(file_path_sdi, frame_rate)
     start = time.time()
     for i in range(1, num_frames+1):
-        source.get_frame(frame)
+        source_sdi.get_frame(frame_sdi)
         if i % 30 == 0:
             elapsed = (time.time() - start)
             print 'Frame ' + str(i) + ', ' + \
                   str(timedelta(seconds=elapsed)) + \
                   ', ' + str(num_frames)
-        target.append(frame)
+        target_sdi.append(frame_sdi)
     elapsed = (time.time() - start)
     print 'TOTAL ' + \
           str(num_frames) + ' @ ' + \
-          str(frame.cols()) + ' x ' + str(frame.rows()) + \
+          str(frame_sdi.cols()) + ' x ' + str(frame_sdi.rows()) + \
           ' (Epiphan SDI), ' + str(timedelta(seconds=elapsed))
-    target.finalise()
+    target_sdi.finalise()
     pygiftgrab.Factory.disconnect(pygiftgrab.Device.DVI2PCIeDuo_SDI)
 except (RuntimeError, IOError) as e:
     print e.message
