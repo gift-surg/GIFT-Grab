4a2a122c0e2317ffb9b82bdf72949f25b9da063d
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-04 09:24:48 +0100

    Issue #25: recording both SDI and DVI ports in python test script, and also more verbose reports produced

--- src/tests/save_frames.py
+++ src/tests/save_frames.py
@@ -1,41 +1,87 @@
 #!/usr/bin/python
 
+# imports
 import pygiftgrab
 import time
 from datetime import timedelta
 
+# passed parameters
 frame_rate = 60
 recording_duration = 1  # min
 num_frames = int(recording_duration * 60 * frame_rate)
-device_type = pygiftgrab.Device.DVI2PCIeDuo_SDI
 storage_type = pygiftgrab.Storage.File_H265
+lap = 30
+
+# derived parameters
 if storage_type == pygiftgrab.Storage.File_H265:
     extension = '.mp4'
 elif storage_type == pygiftgrab.Storage.File_XviD:
     extension = '.avi'
-file_path_sdi = str(recording_duration) + '-min-python-recording-from-' + str(device_type) + extension
+
+file_path_sdi = str(recording_duration) + \
+                '-min-python-recording-from-' + \
+                str(pygiftgrab.Device.DVI2PCIeDuo_SDI) + \
+                extension
+file_path_dvi = str(recording_duration) + \
+                '-min-python-recording-from-' + \
+                str(pygiftgrab.Device.DVI2PCIeDuo_DVI) + \
+                extension
 
 frame_sdi = pygiftgrab.VideoFrame_BGRA(False)  # to avoid "thin wrappers" required for default args
+frame_dvi = pygiftgrab.VideoFrame_BGRA(False)
 try:
-    source_sdi = pygiftgrab.Factory.connect(device_type)
-    # source.set_sub_frame(660, 160, 678, 688)
+    # connect to both ports
+    source_sdi = pygiftgrab.Factory.connect(pygiftgrab.Device.DVI2PCIeDuo_SDI)
+    source_dvi = pygiftgrab.Factory.connect(pygiftgrab.Device.DVI2PCIeDuo_DVI)
+
+    # set sub frames
+    x = 660
+    y = 160
+    width = 678
+    height = 688
+    source_sdi.set_sub_frame(x, y, width, height)
+    source_dvi.set_sub_frame(x, y, width, height)
+
+    # initialise file writers
     target_sdi = pygiftgrab.Factory.writer(storage_type)
     target_sdi.init(file_path_sdi, frame_rate)
+    target_dvi = pygiftgrab.Factory.writer(storage_type)
+    target_dvi.init(file_path_dvi, frame_rate)
+
+    # actual work
     start = time.time()
     for i in range(1, num_frames+1):
         source_sdi.get_frame(frame_sdi)
-        if i % 30 == 0:
+        source_dvi.get_frame(frame_dvi)
+        if i % lap == 0:
             elapsed = (time.time() - start)
-            print 'Frame ' + str(i) + ', ' + \
+            print 'Frame, ' + str(i) + ', ' + \
                   str(timedelta(seconds=elapsed)) + \
                   ', ' + str(num_frames)
         target_sdi.append(frame_sdi)
+        target_dvi.append(frame_dvi)
     elapsed = (time.time() - start)
-    print 'TOTAL ' + \
-          str(num_frames) + ' @ ' + \
-          str(frame_sdi.cols()) + ' x ' + str(frame_sdi.rows()) + \
-          ' (Epiphan SDI), ' + str(timedelta(seconds=elapsed))
+
+    # report
+    total_num_frames = 2*num_frames
+    real_frame_rate = num_frames / elapsed
+    print 'TOTAL Epiphan frames, total time, total expected time, ' + \
+          'real framerate, expected framerate, SDI w x h, DVI w x h, ' + \
+          str(total_num_frames) + ', ' + \
+          str(timedelta(seconds=elapsed)) + ', ' + \
+          str(timedelta(minutes=recording_duration)) + ', ' + \
+          str(real_frame_rate) + ', ' + \
+          str(frame_rate) + ', ' + \
+          str(frame_sdi.cols()) + ', ' + str(frame_sdi.rows()) + ', ' + \
+          str(frame_dvi.cols()) + ', ' + str(frame_dvi.rows())
+
+    # finalise recording
     target_sdi.finalise()
+    target_dvi.finalise()
+
+    # disconnect from ports
     pygiftgrab.Factory.disconnect(pygiftgrab.Device.DVI2PCIeDuo_SDI)
+    pygiftgrab.Factory.disconnect(pygiftgrab.Device.DVI2PCIeDuo_DVI)
+
 except (RuntimeError, IOError) as e:
     print e.message
