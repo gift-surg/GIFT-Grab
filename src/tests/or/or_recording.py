92e9f0aba37f700a56fb67173b7815ebee322b57
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-11 12:51:36 +0100

    Issue #35: added support for filepath in thread constructor, and also using one file per recording session (i.e. pause-resume)

--- src/tests/or/or_recording.py
+++ src/tests/or/or_recording.py
@@ -3,8 +3,12 @@
 from or_thread import ORThread
 from time import sleep
 
-fetoscope_thread = ORThread(frame_rate=27)
+fetoscope_thread = ORThread(frame_rate=27, file_path='test-dvi')
 fetoscope_thread.start()
+sleep(5)
+fetoscope_thread.pause_recording()
+sleep(4)
+fetoscope_thread.resume_recording()
 sleep(5)
 fetoscope_thread.pause_recording()
 sleep(4)
--- src/tests/or/or_thread.py
+++ src/tests/or/or_thread.py
@@ -5,8 +5,11 @@ from time import sleep, time
 
 
 class ORThread(Thread):
-    def __init__(self, frame_rate):
+    def __init__(self, frame_rate, file_path):
         print 'init'
+        # TODO - filename health checks
+        self.file_path = file_path
+        self.recording_index = 0
         # TODO - frame_rate health checks
         self.frame_rate = frame_rate
         self.is_running = True
@@ -16,16 +19,18 @@ class ORThread(Thread):
     def run(self):
         self.is_recording = True
         inter_frame_duration = 1.0 / self.frame_rate # sec
+        print 'filename: ' + self.__next_filename()
         print 'inter-frame duration: ' + str(inter_frame_duration) + ' sec'
         while self.is_running:
             current_processing_start = time()
-            print 'run'
-            sleep(0.002)
+            # print 'run'
+            sleep(0.02)
             # TODO
             if self.is_recording:
-                print 'record'
+                # TODO
+                2 + 2
             elapsed = time() - current_processing_start
-            print 'elapsed ' + str(elapsed) + ' sec'
+            # print 'elapsed ' + str(elapsed) + ' sec'
             sleep(inter_frame_duration - elapsed)
 
     def stop(self):
@@ -33,7 +38,13 @@ class ORThread(Thread):
         self.is_running = False
 
     def pause_recording(self):
+        # TODO finalise current file
         self.is_recording = False
 
     def resume_recording(self):
+        print 'new filename: ' + self.__next_filename()
         self.is_recording = True
+
+    def __next_filename(self):
+        self.recording_index += 1
+        return self.file_path + '-' + '{0:06d}'.format(self.recording_index) + '.mp4'
