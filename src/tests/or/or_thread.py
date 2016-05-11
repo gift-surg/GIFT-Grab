6f347d590857cd9e4a7171b321a3c5cf75f4f564
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-11 11:50:18 +0100

    Issue #35: added simple pause/resume-recording mechanism

--- src/tests/or/or_thread.py
+++ src/tests/or/or_thread.py
@@ -8,14 +8,24 @@ class ORThread(Thread):
     def __init__(self):
         print 'init'
         self.is_running = True
+        self.is_recording = False
         Thread.__init__(self)
 
     def run(self):
+        self.is_recording = True
         while self.is_running:
             print 'run'
             sleep(0.5)
             # TODO
+            if self.is_recording:
+                print 'record'
 
     def stop(self):
         print 'stop'
         self.is_running = False
+
+    def pause_recording(self):
+        self.is_recording = False
+
+    def resume_recording(self):
+        self.is_recording = True
