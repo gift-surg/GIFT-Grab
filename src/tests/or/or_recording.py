416247c541af0f951867e0682c080254688f23b0
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-13 14:34:48 +0100

    Issue #35: avoid overwriting existing data by creating a uniquely named (timestamped) folder for each session

--- src/tests/or/or_recording.py
+++ src/tests/or/or_recording.py
@@ -1,14 +1,43 @@
 #!/usr/bin/env python
 
 from epiphan import EpiphanRecorder
-from time import sleep
+from time import sleep, strftime
+from os.path import exists
+from os import makedirs
+from random import choice
+from string import ascii_uppercase
 import pygiftgrab
 
+
+def session_folder():
+    attempts = 0
+    while attempts < 5:
+        attempts += 1
+        unique_suffix = ''.join(choice(ascii_uppercase) for _ in range(5))
+        folder = strftime('%Y-%m-%d-%H-%M-%S') + '-' +\
+                 unique_suffix + '/'
+
+        if not exists(folder):
+            makedirs(folder)
+            return True, folder
+        else:
+            print 'Folder ' + folder + \
+                  ' exists, will make a ' + \
+                  str(attempts) + '. attempt'
+    print 'Made ' + str(attempts) + \
+          ' unsuccessful attempts to create ' + \
+          'a uniquely-named folder'
+    return False, folder
+
 if __name__ == '__main__':
+    ret, folder = session_folder()
+    if not ret:
+        exit(1)
+
     fs = EpiphanRecorder(port=pygiftgrab.Device.DVI2PCIeDuo_SDI,
-                         frame_rate=27, file_path='test-fetoscope')
+                         frame_rate=27, file_path=folder+'test-fetoscope')
     us = EpiphanRecorder(port=pygiftgrab.Device.DVI2PCIeDuo_DVI,
-                         frame_rate=15, file_path='test-us')
+                         frame_rate=15, file_path=folder+'test-us')
     fs.start()
     us.start()
     sleep(5)
