d2daac4079ec214372514e21bef5e84c99617eb2
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-13 10:36:41 +0100

    Issue #35: revised and verbosified documentation

--- src/tests/or/epiphan.py
+++ src/tests/or/epiphan.py
@@ -12,16 +12,16 @@ class EpiphanRecorder(Thread):
     """A video recording thread that can be started, paused, resumed, and stopped.
 
     When everything just works, this thread will connect to either the DVI or SDI
-    port of an Epiphan DVI2PCIe Duo, grab frames there at the specified
-    ``frame_rate``, and save them using H265 (HEVC) encoding in an MP4 container
-    pointed to by the specified ``file_path``, which serves only as a prefix, i.e.
+    `port` of an Epiphan DVI2PCIe Duo, grab frames there at the specified
+    `frame_rate`, and save them using H265 (HEVC) encoding in an MP4 container
+    pointed to by the specified `file_path`, which serves only as a prefix, i.e.
     for each resume-pause a new file will be created with a recording index and
     the file extension appended. After each video file has been finalised, a text
     file will be created as well that will contain the total latency for that
     video file.
 
     This thread class will attempt to perform the requested operations within a
-    timeout limit. As such, after the timeout, the operations will fail and this
+    `timeout_limit`. As such, after the timeout, the operations will fail and this
     will be propagated to the client through error messages. The thread will
     simply stop at this point. Note that this thread class does not try to be
     intelligent at all. As such, it is the client's responsibility to properly
@@ -32,15 +32,17 @@ class EpiphanRecorder(Thread):
     def __init__(self, port, frame_rate, file_path):
         """Initialise thread with desired configuration.
 
-        @param port ``pygiftgrab.Device.DVI2PCIeDuo_SDI`` or
-        ``pygiftgrab.Device.DVI2PCIeDuo_DVI``
+        Thread will not run if file writer cannot be created.
+
+        @param port `pygiftgrab.Device.DVI2PCIeDuo_SDI` or
+        `pygiftgrab.Device.DVI2PCIeDuo_DVI`
         @param frame_rate Epiphan DVI2PCIe Duo supports up to 60 fps
         at ``1920 x 1080``, when using a single port, and 37 fps when using
         both ports (when capturing in the RGB24 colour space)
         @param file_path any referenced directories must exist and be
         writable by user
-        @return thread will not run if file writer cannot be created
         """
+
         self.port = port
         try:
             self.file = pygiftgrab.Factory.writer(pygiftgrab.Storage.File_H265)
@@ -60,9 +62,9 @@ class EpiphanRecorder(Thread):
         Thread.__init__(self)
 
     def run(self):
-        """Connect to specified ``port`` and start looping until `stop()`ped.
+        """Connect to specified `port` and start looping until `stop()`ped.
 
-        @return Will simply not do anything ``if not self.is_running``
+        Will simply do nothing if not `is_running`.
         """
 
         # mitigate re-start risk
@@ -101,9 +103,9 @@ class EpiphanRecorder(Thread):
                 self.latency -= sleep_duration
 
     def stop(self):
-        """Stop a ``start()``ed thread.
+        """Tell a `run()`ning thread to stop.
 
-        @return Will simply not do anything ``if not self.is_running``
+        Will simply do nothing if not `is_running`.
         """
         if not self.is_running:
             return
@@ -117,11 +119,11 @@ class EpiphanRecorder(Thread):
             print e.message
 
     def pause_recording(self):
-        """Tell a ``start()``ed thread to pause recording.
+        """Tell a `run()`ning thread to pause recording.
 
-        This will finalise the current video file.
+        This will finalise the current video `file`.
 
-        @return
+        @see resume_recording()
         """
         # TODO - this line is quick hack because of GiftGrab#37
         if not self.is_running:
@@ -149,11 +151,11 @@ class EpiphanRecorder(Thread):
         self.latency = 0.00
 
     def resume_recording(self):
-        """Tell a ``start()``ed thread to resume recording.
+        """Tell a `run()`ning and paused thread to resume recording.
 
-        This will create and open a new video file.
+        This will create and open a new video `file`.
 
-        @return
+        @see pause_recording()
         """
         # TODO - this line is quick hack because of GiftGrab#37
         if not self.is_running:
@@ -181,17 +183,16 @@ class EpiphanRecorder(Thread):
             self.is_recording = True
 
     def set_sub_frame(self, x, y, width, height):
-        """Set which region of interest to record.
+        """Set region of interest to record.
 
         This will have no effect if called in the middle of
         a recording. Also no safety checks are made for the
-        passed values.
+        boundaries.
 
-        @param x any
-        @param y
+        @param x upper-left corner coordinate
+        @param y upper-left corner coordinate
         @param width
         @param height
-        @return
         """
         if not self.is_recording:
             self.sub_frame = [x, y, width, height]
@@ -201,8 +202,6 @@ class EpiphanRecorder(Thread):
 
         This will have no effect if called in the middle of
         a recording.
-
-        @return
         """
         if not self.is_recording:
             self.sub_frame = None
@@ -211,12 +210,12 @@ class EpiphanRecorder(Thread):
         """Report what video file to record to.
 
         Automatically generate the full file path from
-        ``file_path`` and ``recording_index``.
+        `file_path` and `recording_index`.
 
         @param increment_index if ``True``, then increments
         index as well, i.e. use ``False`` only when checking
         what video file currently recording to.
-        @return
+        @return e.g. ``file_path-000003.mp4``
         """
         if increment_index:
             self.recording_index += 1
@@ -225,7 +224,7 @@ class EpiphanRecorder(Thread):
             '.mp4'
 
     def __inter_frame_duration(self):
-        """Compute time needed between frames based on ``frame_rate``.
+        """Compute time needed between frames based on `frame_rate`.
 
         @return
         """
