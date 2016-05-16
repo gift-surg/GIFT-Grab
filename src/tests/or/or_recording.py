652fdde57cf5d245e3a799055f6adf02c8e5d068
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-16 12:38:21 +0100

    Issue #35: handling all exception types in recording script

--- src/tests/or/or_recording.py
+++ src/tests/or/or_recording.py
@@ -2,41 +2,56 @@
 
 from epiphan import parse, dump
 from time import sleep
+import yaml
 
 
 if __name__ == '__main__':
     # create sessions
     fs_config = 'sdi.yml'
-    fs = parse(fs_config)
-
     us_config = 'dvi.yml'
-    us = parse(us_config)
 
-    # do actual work
-    fs.start()
-    us.start()
-    sleep(5)
-    fs.pause_recording()
-    us.pause_recording()
-    sleep(4)
-    fs.set_sub_frame(426, 40, 1067, 1040)
-    fs.resume_recording()
-    us.resume_recording()
-    sleep(5)
-    fs.pause_recording()
-    us.pause_recording()
-    sleep(4)
-    fs.set_full_frame()
-    fs.resume_recording()
-    us.resume_recording()
-    sleep(5)
+    try:
+        fs = parse(fs_config)
+        us = parse(us_config)
+    except (yaml.YAMLError, IOError, ValueError) as e:
+        print 'Parsing configuration files failed with: ' + \
+              e.message
+    else:
+        # do actual work
+        fs.start()
+        us.start()
+        sleep(5)
+        fs.pause_recording()
+        us.pause_recording()
+        sleep(4)
+        fs.set_sub_frame(426, 40, 1067, 1040)
+        fs.resume_recording()
+        us.resume_recording()
+        sleep(5)
+        fs.pause_recording()
+        us.pause_recording()
+        sleep(4)
+        fs.set_full_frame()
+        fs.resume_recording()
+        us.resume_recording()
+        sleep(5)
 
-    # finish session
-    fs.stop()
-    us.stop()
-    fs.join()
-    us.join()
+        # finish session
+        fs.stop()
+        us.stop()
 
-    # dump configuration
-    dump(fs)
-    dump(us)
+        # join threads
+        fs.join(timeout=10)
+        if fs.isAlive():
+            print 'Fetoscope thread could not be stopped'
+        us.join(timeout=10)
+        if us.isAlive():
+            print 'Ultrasound thread could not be stopped'
+
+        # dump configuration
+        try:
+            dump(fs)
+            dump(us)
+        except (yaml.YAMLError, IOError) as e:
+            print 'Dumping configuration failed with: ' + \
+                  e.message
