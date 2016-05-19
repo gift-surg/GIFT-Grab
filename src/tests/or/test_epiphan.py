c60c4797b9751a4354fa5c66ea19a7672599fc4f
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-19 17:32:59 +0100

    Issue #46: testing output directory created, and threads joined and terminated properly after first start-pause

--- src/tests/or/test_epiphan.py
+++ src/tests/or/test_epiphan.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-from epiphan import parse
+from time import sleep
 from pytest import raises
 import yaml
+from os.path import isdir, dirname
+from epiphan import parse
 import pygiftgrab
 
 
@@ -36,3 +38,23 @@ def test_frame_grabbing():
     assert us.frame_rate == 14.0 and \
         us.port == pygiftgrab.Device.DVI2PCIeDuo_DVI and \
         us.timeout_limit == 15.0
+
+    # test actual output now
+    fs.start()
+    us.start()
+    sleep(5)
+    fs.pause_recording()
+    us.pause_recording()
+    sleep(4)
+    assert isdir(dirname(fs.file_path))
+    assert isdir(dirname(us.file_path))
+
+    # finish session
+    fs.stop()
+    us.stop()
+
+    # join threads
+    fs.join(timeout=fs.timeout_limit)
+    assert not fs.isAlive()
+    us.join(timeout=us.timeout_limit)
+    assert not us.isAlive()
