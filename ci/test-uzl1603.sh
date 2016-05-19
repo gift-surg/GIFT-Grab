d8422ac6a20d591ac4dda39ab989b35cddffa0e6
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-19 15:55:05 +0100

    Issue #46: added custom CI build configuration for UZL 1603 and a dummy py.test script

---
+++ ci/test-uzl1603.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+num_lines=$(/opt/installers/epiphan/sdk/3.30.3.0007/epiphan/samples/v2u/build/release/v2u -l | grep --count "no signal detected")
+if [ $num_lines -gt 0 ]; then
+  echo "No alive device seems to be connected to Epiphan"
+  echo "Please connect both ports of Storz, and retry build"
+  exit 1
+else
+  cd "$GiftGrab_BUILD_DIR"
+  py.test "$GiftGrab_SOURCE_DIR/tests/or/test_epiphan.py"
+fi
+
---
+++ src/tests/or/test_epiphan.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+
+def func(x):
+    return x + 1
+
+
+def test_answer():
+    print 'dummy test bound to pass'
+    assert func(3) == 4
+
