a88ba1f1e2ea01b48b526250089c94b08f31d24d
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-20 17:48:12 +0100

    Issue #31: added generic try-catch block to test app

--- src/tests/epiphan/dvi2pcieduo.cpp
+++ src/tests/epiphan/dvi2pcieduo.cpp
@@ -5,26 +5,34 @@
 int main()
 {
 #ifdef USE_COLOUR_SPACE_I420
-    gg::VideoFrame_I420 frame;
-    enum gg::Device device = gg::Device::DVI2PCIeDuo_SDI;
-    IVideoSource * source = gg::Factory::connect(device);
-    auto started_at = std::chrono::high_resolution_clock::now();
-    int num_frames = 180;
-    for (int i = 0; i < num_frames; )
+    try
     {
-        source->get_frame(frame);
-        std::cout << ++i << ". frame: "
-                  << frame.cols() << " x " << frame.rows()
+        gg::VideoFrame_I420 frame;
+        enum gg::Device device = gg::Device::DVI2PCIeDuo_SDI;
+        IVideoSource * source = gg::Factory::connect(device);
+        auto started_at = std::chrono::high_resolution_clock::now();
+        int num_frames = 180;
+        for (int i = 0; i < num_frames; )
+        {
+            source->get_frame(frame);
+            std::cout << ++i << ". frame: "
+                      << frame.cols() << " x " << frame.rows()
+                      << std::endl;
+        }
+        float elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(
+                        std::chrono::high_resolution_clock::now() - started_at
+                    ).count();
+        std::cout << num_frames << " frames grabbed in " << elapsed_seconds << " sec: "
+                  << (num_frames/elapsed_seconds) << " fps"
                   << std::endl;
+        gg::Factory::disconnect(device);
+        return EXIT_SUCCESS;
     }
-    float elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(
-                std::chrono::high_resolution_clock::now() - started_at
-                ).count();
-    std::cout << num_frames << " frames grabbed in " << elapsed_seconds << " sec: "
-              << (num_frames/elapsed_seconds) << " fps"
-              << std::endl;
-    gg::Factory::disconnect(device);
-    return EXIT_SUCCESS;
+    catch (std::exception & e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
 #else
     return EXIT_SUCCESS;
 #endif
