44b88cd443d38a574241e16702661bf9d836d3ff
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-23 12:40:01 +0100

    Issue #31: factored test code out into function, in preparation of imminent introduction of Pthread-based multi-threading test

--- src/tests/epiphan/dvi2pcieduo.cpp
+++ src/tests/epiphan/dvi2pcieduo.cpp
@@ -2,31 +2,53 @@
 #include <iostream>
 #include <chrono>
 
+void grab(const enum gg::Device device,
+          const size_t num_frames,
+          float & duration)
+{
+    std::string device_str = "";
+    switch (device)
+    {
+    case gg::Device::DVI2PCIeDuo_DVI:
+        device_str = "DVI";
+        break;
+    case gg::Device::DVI2PCIeDuo_SDI:
+        device_str = "SDI";
+        break;
+    default:
+        throw gg::BasicException("Device not set");
+    }
+
+    gg::VideoFrame_I420 frame;
+    IVideoSource * source = gg::Factory::connect(device);
+    auto started_at = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < num_frames; )
+    {
+        source->get_frame(frame);
+        std::cout << device_str << ": "
+                  << ++i << ". frame: "
+                  << frame.cols() << " x " << frame.rows()
+                  << std::endl;
+    }
+    duration = std::chrono::duration_cast<std::chrono::seconds>(
+                    std::chrono::high_resolution_clock::now() - started_at
+                ).count();
+    gg::Factory::disconnect(device);
+}
+
 int main()
 {
 #ifdef USE_COLOUR_SPACE_I420
     try
     {
-        gg::VideoFrame_I420 frame;
-        enum gg::Device device = gg::Device::DVI2PCIeDuo_SDI;
-        IVideoSource * source = gg::Factory::connect(device);
-        auto started_at = std::chrono::high_resolution_clock::now();
-        int num_frames = 180;
-        for (int i = 0; i < num_frames; )
-        {
-            source->get_frame(frame);
-            std::cout << ++i << ". frame: "
-                      << frame.cols() << " x " << frame.rows()
-                      << std::endl;
-        }
-        float elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(
-                        std::chrono::high_resolution_clock::now() - started_at
-                    ).count();
-        float frame_rate = num_frames/elapsed_seconds;
-        std::cout << num_frames << " frames grabbed in " << elapsed_seconds << " sec: "
-                  << frame_rate << " fps"
+        const size_t num_frames = 180;
+        float duration_sdi = 0;
+        grab(gg::Device::DVI2PCIeDuo_SDI, num_frames, duration_sdi);
+        float frame_rate_sdi = num_frames/duration_sdi;
+        std::cout << num_frames << " frames grabbed in "
+                  << duration_sdi << " sec: "
+                  << frame_rate_sdi << " fps"
                   << std::endl;
-        gg::Factory::disconnect(device);
         return EXIT_SUCCESS;
     }
     catch (std::exception & e)
