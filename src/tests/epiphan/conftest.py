d1c91a86ace2fcfc49767836a1768e1797e6931d
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2016-05-31 17:36:22 +0100

    Issue #48: trying passing --colour-space=BGR24 to tests

--- src/tests/epiphan/conftest.py
+++ src/tests/epiphan/conftest.py
@@ -2,17 +2,15 @@ from epiphan import BGR24, I420
 
 
 def pytest_addoption(parser):
-    parser.addoption("--bgr24", action="store_true",
-                     help="Run tests for BGR24 colour space (up to 30 fps)")
-    parser.addoption("--i420", action="store_true",
-                     help="Run tests for I420 colour space (up to 60 fps)")
+    parser.addoption('--colour-space', action='store',
+                     help='Colour space specification (BGR24 or I420)')
 
 
 def pytest_generate_tests(metafunc):
     if 'colour_space' in metafunc.fixturenames:
-        if metafunc.config.option.bgr24:
+        if metafunc.config.option.colour_space == 'BGR24':
             colour_space = BGR24
-        elif metafunc.config.option.i420:
+        elif metafunc.config.option.colour_space == 'I420':
             colour_space = I420
         else:
             colour_space = BGR24  # default
