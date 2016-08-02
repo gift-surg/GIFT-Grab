bcab65678e471b79be0ba144792e486f975ac5f3
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-02 17:52:21 +0100

    Issue #67: added automated fixture to connect to and disconnect from specified port before and after each Epiphan test

--- src/tests/epiphan/dvi2pcieduo/test.py
+++ src/tests/epiphan/dvi2pcieduo/test.py
@@ -1,4 +1,5 @@
 from pytest import fail, yield_fixture
+from pygiftgrab import Factory
 
 source = None
 
@@ -6,11 +7,26 @@ source = None
 @yield_fixture(autouse=True)
 def peri_test(port):
     # This section runs before each test
+    global source
+    try:
+        source = Factory.connect(port)
+    except IOError as e:
+        raise RuntimeError('Could not connect to Epiphan DVI2PCIe Duo,\n' +
+                           'The detailed error was:\n' +
+                           e.message)
+    assert source is not None
 
     # Run test
     yield
 
     # This section runs after each test
+    try:
+        Factory.disconnect(port)
+    except IOError as e:
+        raise RuntimeError('Could not disconnect from Epiphan DVI2PCIe Duo,\n' +
+                           'The detailed error was:\n' +
+                           e.message)
+    assert source is None
 
 
 def test_get_frame(port):
