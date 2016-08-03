b19e29b2296ce2b1c8b19d4e2c9c64069e0f600c
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-03 16:18:20 +0100

    Issue #67: EpiphanSDK_DIR set in main CI script, rather than in shell script

--- ci/build-uzl1603-i420.sh
+++ ci/build-uzl1603-i420.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$GiftGrab_BUILD_DIR"
 cmake -D USE_COLOUR_SPACE_I420=ON \
-      -D EpiphanSDK_DIR=/home/gitlab-runner/environments/giftgrab/opt/include/epiphansdk \
+      -D EpiphanSDK_DIR=${EpiphanSDK_DIR} \
       .
 make -j
 make install
