8f352ecc65ed79b044a3a0e0a4b773782d3284fd
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-04 11:39:00 +0100

    Issue #67: all real-time test related stuff under epiphan/dvi2pcieduo now

--- ci/test-uzl1603.sh
+++ ci/test-uzl1603.sh
@@ -6,9 +6,9 @@ if [ $num_lines -gt 0 ]; then
 else
   cd "$GiftGrab_BUILD_DIR"
   cp "$GiftGrab_SOURCE_DIR/python/modules/epiphan.py" ./
-  cp "$GiftGrab_SOURCE_DIR/tests/epiphan/$DVI2PCIEDUO_REALTIME" ./
-  cp "$GiftGrab_SOURCE_DIR/tests/epiphan/conftest.py" ./
-  cp --recursive "$GiftGrab_SOURCE_DIR/tests/epiphan/config" ./
+  cp "$GiftGrab_SOURCE_DIR/tests/epiphan/dvi2pcieduo/$DVI2PCIEDUO_REALTIME" ./
+  cp "$GiftGrab_SOURCE_DIR/tests/epiphan/dvi2pcieduo/conftest.py" ./
+  cp --recursive "$GiftGrab_SOURCE_DIR/tests/epiphan/dvi2pcieduo/config" ./
   cp "$GiftGrab_SOURCE_DIR/python/modules/example_epiphan/"* ./config
 
   py.test --colour-space=BGR24 $DVI2PCIEDUO_REALTIME
--- src/tests/epiphan/dvi2pcieduo/conftest.py
+++ src/tests/epiphan/dvi2pcieduo/conftest.py
@@ -2,11 +2,23 @@ from pygiftgrab import Device
 
 
 def pytest_addoption(parser):
+    parser.addoption('--colour-space', action='store',
+                     help='Colour space specification (BGR24 or I420)')
     parser.addoption('--port', action='store',
                      help='Epiphan DVI2PCIe Duo port specification (SDI or DVI)')
 
 
 def pytest_generate_tests(metafunc):
+    if 'colour_space' in metafunc.fixturenames:
+        if metafunc.config.option.colour_space == 'BGR24':
+            colour_space = BGR24
+        elif metafunc.config.option.colour_space == 'I420':
+            colour_space = I420
+        else:
+            raise RuntimeError('Could not recognise colour space ' +
+                               metafunc.config.option.colour_space)
+        metafunc.parametrize('colour_space', [colour_space])
+
     if 'port' in metafunc.fixturenames:
         if metafunc.config.option.port == 'SDI':
             port = Device.DVI2PCIeDuo_DVI
