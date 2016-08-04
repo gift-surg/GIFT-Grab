51748a20d5cedc812c9c19f2a4e04762e7b9ec2d
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-04 18:27:10 +0100

    Issue #65: checking exit status of make command in Linux CI build script, as it seems to continue even after failing make's

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -3,40 +3,49 @@ rm -rf "$GiftGrab_BUILD_DIR"
 mkdir -p "$GiftGrab_BUILD_DIR"
 cd "$GiftGrab_BUILD_DIR"
 
+# so that CI script will fail when e.g. make -j fails:
+function exit_on_fail {
+    exit_status=$?
+    if [[ "$exit_status" -ne "0" ]]; then
+        echo "Build failed due to last exit status being $exit_status"
+        exit $exit_status
+    fi
+}
+
 # bare-bones build
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -D BUILD_DOC=ON \
       "$GiftGrab_SOURCE_DIR"
-make -j
+make -j; exit_on_fail
 make install
 
 # Python and tests (some of which depend on python)
 cmake -D BUILD_PYTHON=ON \
       -D BUILD_TESTS=ON \
       .
-make -j
+make -j; exit_on_fail
 make install
 
 # H265 support
 cmake -D USE_H265=ON \
       -D FFmpeg_DIR=$FFmpeg_DIR \
       .
-make -j
+make -j; exit_on_fail
 make install
 
 # Xvid support (OpenCV should get switched here)
 cmake -D USE_XVID=ON .
-make -j
+make -j; exit_on_fail
 make install
 
 # Epiphan DVI2PCIe Duo support (OpenCV)
 cmake -D USE_EPIPHAN_DVI2PCIE_DUO=ON .
-make -j
+make -j; exit_on_fail
 make install
 
 # Epiphan DVI2PCIe Duo support (EpiphanSDK)
 cmake -D USE_COLOUR_SPACE_I420=ON \
       -D EpiphanSDK_DIR=$EpiphanSDK_DIR \
       .
-make -j
+make -j; exit_on_fail
 make install
