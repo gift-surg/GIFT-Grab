7542fed2dcb8589c9510c205bab7556e1778a33d
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-04 17:21:50 +0100

    Issue #65: CI script for general Linux builds encompasses all supported build configurations now

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -2,9 +2,41 @@
 rm -rf "$GiftGrab_BUILD_DIR"
 mkdir -p "$GiftGrab_BUILD_DIR"
 cd "$GiftGrab_BUILD_DIR"
+
+# bare-bones build
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -D BUILD_DOC=ON \
-      -D BUILD_PYTHON=ON \
       "$GiftGrab_SOURCE_DIR"
+make -j
+make install
+
+# Python and tests (some of which depend on python)
+cmake -D BUILD_PYTHON=ON \
+      -D BUILD_TESTS=ON \
+      .
+make -j
+make install
+
+# H265 support
+cmake -D USE_H265=ON \
+      -D FFmpeg_DIR=$FFmpeg_DIR \
+      .
+make -j
+make install
+
+# Xvid support (OpenCV should get switched here)
+cmake -D USE_XVID=ON .
+make -j
+make install
+
+# Epiphan DVI2PCIe Duo support (OpenCV)
+cmake -D USE_EPIPHAN_DVI2PCIE_DUO=ON .
+make -j
+make install
+
+# Epiphan DVI2PCIe Duo support (EpiphanSDK)
+cmake -D USE_COLOUR_SPACE_I420=ON \
+      -D EpiphanSDK_DIR=$EpiphanSDK_DIR \
+      .
 make -j
 make install
