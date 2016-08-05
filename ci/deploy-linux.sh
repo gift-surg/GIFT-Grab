78749d29cc8aaa62ef6d7bf97ef26b750cf38693
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-05 17:52:36 +0100

    Issue #65: providing GiftGrab_DIR through CMAKE_MODULE_PATH in CI scripts

--- ci/deploy-linux.sh
+++ ci/deploy-linux.sh
@@ -22,6 +22,7 @@ rm -rf "$GpuMosaicing_BUILD_DIR"
 mkdir -p "$GpuMosaicing_BUILD_DIR"
 cd "$GpuMosaicing_BUILD_DIR"
 cmake -D NiftyMatch_DIR="$NiftyMatch_DIR" \
+      -D CMAKE_MODULE_PATH="$GiftGrab_DIR" \
       -D Qt5Widgets_DIR="$Qt5Widgets_DIR" \
       -D Qt5OpenGL_DIR="$Qt5OpenGL_DIR" \
       -D CMAKE_BUILD_TYPE=Release \
