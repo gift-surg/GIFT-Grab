513891f835a312bfcb05d57f248e36cc7aeaf967
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-09 12:17:53 +0100

    Issue #78: added ctest to gift-linux CI builds

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -15,39 +15,49 @@ function exit_on_fail {
 # bare-bones build
 cmake -D CMAKE_INSTALL_PREFIX="$INSTALL_DIR" "$GiftGrab_SOURCE_DIR"
 make -j; exit_on_fail
+# no ctest: no features activated to be tested yet
 make install
 
 # build docs
 cmake -D BUILD_DOC=ON .
 make -j; exit_on_fail
+# no ctest: no features activated to be tested yet
 make install
 
 # Python and tests (currently all python)
 cmake -D BUILD_PYTHON=ON -D BUILD_TESTS=ON .
 make -j; exit_on_fail
+# no ctest: no features activated to be tested yet
 make install
 
 # H265 support
 cmake -D USE_H265=ON .
 make -j; exit_on_fail
+ctest
 make install
 
 # hardware-accelerated H265
 cmake -D USE_NVENC=ON .
 make -j; exit_on_fail
+ctest
 make install
 
 # Xvid support (OpenCV should get switched here)
 cmake -D USE_XVID=ON .
 make -j; exit_on_fail
+ctest
 make install
 
 # Epiphan DVI2PCIe Duo support (OpenCV)
 cmake -D USE_EPIPHAN_DVI2PCIE_DUO=ON .
 make -j; exit_on_fail
+# no ctest for Epiphan DVI2PCIe Duo on
+# general-purpose Linux test server
 make install
 
 # Epiphan DVI2PCIe Duo support (EpiphanSDK)
 cmake -D USE_I420=ON .
 make -j; exit_on_fail
+# no ctest for Epiphan DVI2PCIe Duo on
+# general-purpose Linux test server
 make install
