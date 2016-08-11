155fb25a14b95215463808cbef462a075d088820
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-11 16:16:21 +0100

    Issue #68: C++ library libgiftgrab instead of pygiftgrab and more structured setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, Extension
 
-pygiftgrab = Extension('pygiftgrab',
-                       sources=['api/videoframe.cpp',
-                        'api/maskframe.cpp',
-                        'api/factory.cpp',
-                        'api/except.cpp'],
-                        include_dirs=['../src/api', '../src/utils'],
+lib_sources = ['api/videoframe.cpp',
+               'api/maskframe.cpp',
+               'api/factory.cpp',
+               'api/except.cpp']
+lib_include_dirs = ['api', 'utils']
+libgiftgrab = Extension('libgiftgrab',
+                        sources=lib_sources,
+                        include_dirs=lib_include_dirs,
                         extra_compile_args=['-std=c++11', '-O3', '-DNDEBUG', '-fPIC'],
                         extra_link_flags=['-fPIC', '-std=c++11', '-O3', '-DNDEBUG', '-shared'])
 
 setup(
     name='giftgrab',
     version='16.08.11rc1',
-    ext_modules=[pygiftgrab]
+    ext_modules=[libgiftgrab]
     )
