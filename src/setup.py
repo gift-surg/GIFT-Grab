886126e87ec098a546cc6adb5c5a0735ca9141b8
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-12 10:08:53 +0100

    Issue #68: generation of dir strings automated based on python version, and removed appending of local dir

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, Extension
-from distutils.command.build import build
 from sys import version_info, exec_prefix
 from sysconfig import get_platform
 from os.path import join
@@ -15,7 +14,7 @@ extra_compile_args = ['-std=c++11', '-O3', '-DNDEBUG', '-fPIC']
 extra_link_args = ['-fPIC', '-std=c++11', '-O3', '-DNDEBUG', '-shared']
 
 py_sources = ['python/wrapper.cpp']
-py_include_dirs = lib_include_dirs + ['/usr/include/python2.7']
+py_include_dirs = lib_include_dirs + ['/usr/include/python%d.%d' % (version_info.major, version_info.minor)]
 libgiftgrab = Extension('libgiftgrab',
                         sources=lib_sources,
                         include_dirs=lib_include_dirs,
@@ -23,13 +22,13 @@ libgiftgrab = Extension('libgiftgrab',
                         extra_link_flags=extra_link_args
                         )
 py_runtime_library_dirs = [join(
-                              join(join(exec_prefix, 'local/lib'),
+                              join(join(exec_prefix, 'lib'),
                                 'python%d.%d' % (version_info.major, version_info.minor)
                                 ), 'site-packages')]
 py_library_dirs = [join('build',
                         'lib.%s-%d.%d' % (get_platform(), version_info.major, version_info.minor))]
-print py_library_dirs
-print py_runtime_library_dirs
+print '>>>lib  %s' % (py_library_dirs)
+print '>>>RT %s' % (py_runtime_library_dirs)
 pygiftgrab = Extension('pygiftgrab',
                        sources=py_sources,
                        include_dirs=py_include_dirs,
