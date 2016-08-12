d7b9b6cc9f5ee6e7858aa24c893a97680008cef4
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-12 14:35:44 +0100

    Issue #68: fixed indentation of setup.py

--- src/setup.py
+++ src/setup.py
@@ -18,22 +18,25 @@ builder = Builder(None)
 
 # TODO: BUILD_TESTS
 
-libgiftgrab = Extension(name=builder.lib_name(),
-                        sources=builder.lib_sources(),
-                        include_dirs=builder.lib_include_dirs(),
-                        extra_compile_args=builder.lib_compile_args(),
-                        extra_link_args=builder.lib_link_args()
-                        )
+libgiftgrab = Extension(
+    name=builder.lib_name(),
+    sources=builder.lib_sources(),
+    include_dirs=builder.lib_include_dirs(),
+    extra_compile_args=builder.lib_compile_args(),
+    extra_link_args=builder.lib_link_args()
+    )
 
-pygiftgrab = Extension(name=builder.py_name(),
-                       sources=builder.py_sources(),
-                       include_dirs=builder.py_include_dirs(),
-                       extra_compile_args=builder.py_compile_args(),
-                       extra_link_args=builder.py_link_args(),
-                       libraries=builder.py_libraries(),
-                       library_dirs=builder.py_library_dirs(),
-                       runtime_library_dirs=builder.py_runtime_library_dirs(),
-                       )
+pygiftgrab = Extension(
+    name=builder.py_name(),
+    sources=builder.py_sources(),
+    include_dirs=builder.py_include_dirs(),
+    extra_compile_args=builder.py_compile_args(),
+    extra_link_args=builder.py_link_args(),
+    libraries=builder.py_libraries(),
+    library_dirs=builder.py_library_dirs(),
+    runtime_library_dirs=builder.py_runtime_library_dirs(),
+    )
+
 setup(
     name='giftgrab',
     version='16.08.11rc1',
