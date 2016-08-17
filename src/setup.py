86372e71339947bb3db8086198d3b982b3e4f68e
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-08-17 12:51:06 +0100

    Issue #68: factored site packages discovery code out into function

--- src/setup.py
+++ src/setup.py
@@ -6,13 +6,32 @@ from os import mkdir, chdir, listdir, getcwd, symlink, rename
 from os.path import join, abspath, dirname
 from subprocess import check_output
 from sys import stderr
-import sys
-venv = hasattr(sys, 'real_prefix')
-if not venv:
-    # see https://github.com/pypa/virtualenv/issues/355
-    from site import getsitepackages
-else:
-    from sys import exec_prefix, version_info
+
+
+def get_pip_root():
+    """Get pip root directory, in different ways,
+    depending on whether running from within a
+    virtualenv.
+
+    @return
+    """
+
+    import sys
+    venv = hasattr(sys, 'real_prefix')
+
+    if not venv:
+        # see https://github.com/pypa/virtualenv/issues/355
+        from site import getsitepackages
+        site_packages = getsitepackages()[0]
+    else:
+        from sys import exec_prefix, version_info
+        site_packages = 'python%d.%d' % (version_info.major,
+                                         version_info.minor)
+        site_packages = join('lib', site_packages)
+        site_packages = join(site_packages, 'site-packages')
+        site_packages = join(exec_prefix, site_packages)
+
+    return site_packages
 
 
 class GiftGrabInstallCommand(install):
@@ -263,16 +282,7 @@ class GiftGrabInstallCommand(install):
         pylib_path = join(join(join(cmake_install_prefix, 'lib'),
                                'giftgrab'),
                           'pygiftgrab.so')
-        if not venv:
-            site_packages = getsitepackages()[0]
-        else:
-            # see https://github.com/pypa/virtualenv/issues/355
-            site_packages = 'python%d.%d' % (version_info.major,
-                                             version_info.minor)
-            site_packages = join('lib', site_packages)
-            site_packages = join(site_packages, 'site-packages')
-            site_packages = join(exec_prefix, site_packages)
-        pylib_link = join(site_packages, 'pygiftgrab.so')
+        pylib_link = join(get_pip_root(), 'pygiftgrab.so')
         print('\n+++++ INFO +++++\n%s%s\n\n' % (
                'Installing GiftGrab with support for: ',
                str(self)
