64ee9148930c4f53a2516d95d1fca4c80ae687f4
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-09-01 11:20:23 +0100

    Issue #83: API methods now up-to-specs (removed exceptions), and also revised use of VLC functions with regards to exception/error handling

--- src/vlc/vlc_video_source.cpp
+++ src/vlc/vlc_video_source.cpp
@@ -105,16 +105,7 @@ void VideoSourceVLC::set_sub_frame(int x, int y, int width, int height)
         y >= _full.y and y + height <= _full.y + _full.height)
     {
         std::string psz_geometry = encode_psz_geometry(x, y, width, height);
-
-        try
-        {
-            libvlc_video_set_crop_geometry(_vlc_mp, psz_geometry.c_str());
-        }
-        catch (...)
-        {
-            throw VideoSourceError(
-                std::string("Could not set sub frame to: ").append(psz_geometry));
-        }
+        libvlc_video_set_crop_geometry(_vlc_mp, psz_geometry.c_str());
     }
 }
 
@@ -125,96 +116,80 @@ void VideoSourceVLC::get_full_frame()
 
     std::string psz_geometry = encode_psz_geometry(_full.x, _full.y,
                                                    _full.width, _full.height);
-    try
-    {
-        libvlc_video_set_crop_geometry(_vlc_mp, psz_geometry.c_str());
-    }
-    catch (...)
-    {
-        throw VideoSourceError(
-            std::string("Could not set full frame"));
-    }
+    libvlc_video_set_crop_geometry(_vlc_mp, psz_geometry.c_str());
 }
 
 
 void VideoSourceVLC::init_vlc(const char * path)
 {
-    try
-    {
-        // VLC pointers
-        libvlc_media_t * vlc_media = nullptr;
+    // VLC pointers
+    libvlc_media_t * vlc_media = nullptr;
 
-        // VLC options
-        char smem_options[512];
+    // VLC options
+    char smem_options[512];
 
-        sprintf(smem_options,
-                "#smem{video-data=%lld,video-prerender-callback=%lld,video-postrender-callback=%lld}",
-                (long long int)(intptr_t)(void*) this,
-                (long long int)(intptr_t)(void*) &VideoSourceVLC::prepareRender,
-                (long long int)(intptr_t)(void*) &VideoSourceVLC::handleStream );
+    sprintf(smem_options,
+            "#smem{video-data=%lld,video-prerender-callback=%lld,video-postrender-callback=%lld}",
+            (long long int)(intptr_t)(void*) this,
+            (long long int)(intptr_t)(void*) &VideoSourceVLC::prepareRender,
+            (long long int)(intptr_t)(void*) &VideoSourceVLC::handleStream );
 
-        const char * const vlc_args[] = {
-            "-I", "dummy", // Don't use any interface
-            "--ignore-config", // Don't use VLC's config
-            "--extraintf=logger", // Log anything
-            // TODO - what about the options below?
-            //"--verbose=2", // Be much more verbose then normal for debugging purpose
-            //"--clock-jitter=0",
-            //"--file-caching=150",
-            "--no-audio",
-            "--sout", smem_options // Stream to memory
-        };
+    const char * const vlc_args[] = {
+        "-I", "dummy", // Don't use any interface
+        "--ignore-config", // Don't use VLC's config
+        "--extraintf=logger", // Log anything
+        // TODO - what about the options below?
+        //"--verbose=2", // Be much more verbose then normal for debugging purpose
+        //"--clock-jitter=0",
+        //"--file-caching=150",
+        "--no-audio",
+        "--sout", smem_options // Stream to memory
+    };
 
-        // We launch VLC
-        _vlc_inst = libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
+    // We launch VLC
+    _vlc_inst = libvlc_new(sizeof(vlc_args) / sizeof(vlc_args[0]), vlc_args);
+    if (_vlc_inst == nullptr)
+        throw VideoSourceError("Could not create VLC engine");
 
-        // If path contains a colon (:), it will be treated as a
-        // URL. Else, it will be considered as a local path.
-        if( std::string(path).find(":") == std::string::npos )
-        {
-            vlc_media = libvlc_media_new_path(_vlc_inst, path);
-        }
-        else
-        {
-            vlc_media = libvlc_media_new_location(_vlc_inst, path);
-        }
+    // If path contains a colon (:), it will be treated as a
+    // URL. Else, it will be considered as a local path.
+    if (std::string(path).find(":") == std::string::npos)
+        vlc_media = libvlc_media_new_path(_vlc_inst, path);
+    else
+        vlc_media = libvlc_media_new_location(_vlc_inst, path);
+    if (vlc_media == nullptr)
+        throw VideoSourceError(std::string("Could not open ").append(path));
 
-        libvlc_media_add_option(vlc_media, ":noaudio");
-        libvlc_media_add_option(vlc_media, ":no-video-title-show");
+    libvlc_media_add_option(vlc_media, ":noaudio");
+    libvlc_media_add_option(vlc_media, ":no-video-title-show");
 
-        // Create a media player playing environement
-        _vlc_mp = libvlc_media_player_new_from_media(vlc_media);
-        // No need to keep the media now
-        libvlc_media_release( vlc_media );
-    }
-    catch (...)
-    {
-        throw VideoSourceError(std::string("Failed to open ").append(path));
-    }
+    // Create a media player playing environement
+    _vlc_mp = libvlc_media_player_new_from_media(vlc_media);
+    if (_vlc_mp == nullptr)
+        throw VideoSourceError("Could create VLC media player");
+
+    // No need to keep the media now
+    libvlc_media_release( vlc_media );
 }
 
 
 void VideoSourceVLC::run_vlc()
 {
-    try
-    {
-        // play the media_player
-        libvlc_media_player_play( _vlc_mp );
-        // empirically determined value that allows for initialisation
-        // to succeed before any API functions are called on this object
-        std::this_thread::sleep_for(std::chrono::milliseconds(350));
-        unsigned int width, height;
-        if (libvlc_video_get_size(_vlc_mp, 0, &width, &height) != 0)
-            throw VideoSourceError("Could not get video dimensions");
-        _full.x = 0;
-        _full.y = 0;
-        _full.width = width;
-        _full.height = height;
-    }
-    catch (...)
-    {
-        throw VideoSourceError("Error while running source");
-    }
+    // play the media_player
+    if (libvlc_media_player_play(_vlc_mp) != 0)
+        throw VideoSourceError("Could not start VLC media player");
+
+    // empirically determined value that allows for initialisation
+    // to succeed before any API functions are called on this object
+    std::this_thread::sleep_for(std::chrono::milliseconds(350));
+    unsigned int width, height;
+    if (libvlc_video_get_size(_vlc_mp, 0, &width, &height) != 0)
+        throw VideoSourceError("Could not get video dimensions");
+
+    _full.x = 0;
+    _full.y = 0;
+    _full.width = width;
+    _full.height = height;
 }
 
 
@@ -226,11 +201,13 @@ void VideoSourceVLC::prepareRender(VideoSourceVLC * p_video_data,
 
     if (size != p_video_data->_data_length)
     {
+        unsigned int width,height;
+        if (libvlc_video_get_size(p_video_data->_vlc_mp, 0, &width, &height) != 0)
+            return;
+
         // TODO deallocate previous data?
         p_video_data->_video_buffer = new uint8_t[size];
         p_video_data->_data_length = size;
-        unsigned int width,height;
-        libvlc_video_get_size(p_video_data->_vlc_mp, 0, &width, &height);
         p_video_data->_cols = width;
         p_video_data->_rows = height;
     }
