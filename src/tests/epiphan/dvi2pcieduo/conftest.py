d2688fdb4207964fdccfad1315943c2a1e0cb37f
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-09-12 12:41:23 +0100

    Fixed Epiphan test configuration script bug confusing DVI and SDI

--- src/tests/epiphan/dvi2pcieduo/conftest.py
+++ src/tests/epiphan/dvi2pcieduo/conftest.py
@@ -23,9 +23,9 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize('colour_space', [colour_space])
 
     if 'port' in metafunc.fixturenames:
-        if metafunc.config.option.port == 'SDI':
+        if metafunc.config.option.port == 'DVI':
             port = Device.DVI2PCIeDuo_DVI
-        elif metafunc.config.option.port == 'DVI':
+        elif metafunc.config.option.port == 'SDI':
             port = Device.DVI2PCIeDuo_SDI
         else:
             raise RuntimeError('Could not recognise Epiphan DVI2PCIe Duo port ' +
