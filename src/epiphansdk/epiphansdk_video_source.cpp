bcc78178211dbd3abb23ca0429228d0eec4a1904
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-11-07 16:56:04 +0000

    Issue #86: Epiphan SDK video source uses colour facility of IVideoSource now

--- src/epiphansdk/epiphansdk_video_source.cpp
+++ src/epiphansdk/epiphansdk_video_source.cpp
@@ -6,9 +6,9 @@ namespace gg
 
 VideoSourceEpiphanSDK::VideoSourceEpiphanSDK(
     const std::string device_id, V2U_INT32 colour_space)
-    : IVideoSource(),
-      _frame_grabber(nullptr),
-      _flags(0)
+    : IVideoSource()
+    , _frame_grabber(nullptr)
+    , _flags(0)
 {
     FrmGrab_Init();
 
@@ -26,9 +26,13 @@ VideoSourceEpiphanSDK::VideoSourceEpiphanSDK(
         std::cerr << "Colour space " << colour_space << " not supported" << std::endl;
         return;
     }
+    else if (colour_space == V2U_GRABFRAME_FORMAT_I420)
+        _colour = I420;
+    else
+        _colour = BGRA;
     _flags |= colour_space;
 
-    VideoFrame frame(I420);
+    VideoFrame frame(_colour);
     _full.x = 0;
     _full.y = 0;
     /* TODO - e.g. EpiphanSDK_MAX_RES_X and
@@ -57,7 +61,7 @@ bool VideoSourceEpiphanSDK::get_frame_dimensions(int & width, int & height)
 
 bool VideoSourceEpiphanSDK::get_frame(VideoFrame & frame)
 {
-    if (frame.colour() != I420)
+    if (frame.colour() != _colour)
         // TODO - exception GiftGrab#42
         return false;
 
