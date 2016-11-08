2d493afb201f4e4f812e0b8c998aad10d0e4da9b
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-11-08 15:58:04 +0000

    Issue #86: added note to IObservable code about duplicate lambda

--- src/api/iobservable.cpp
+++ src/api/iobservable.cpp
@@ -25,6 +25,9 @@ void IObservable::detach(IObserver & observer)
     _observers.erase(std::find_if(
                          _observers.begin(),
                          _observers.end(),
+                         /* Note that this lambda is a duplicate of the one
+                          * used in the attach function below.
+                          */
                          [&](const std::reference_wrapper<IObserver> &o)
                             {
                                 return &(o.get()) == &observer;
@@ -48,6 +51,9 @@ bool IObservable::attached(const IObserver & observer) const noexcept
     return std::find_if(
                 _observers.begin(),
                 _observers.end(),
+                /* Note that this lambda is a duplicate of the one
+                 * used in the detach function above.
+                 */
                 [&](const std::reference_wrapper<IObserver> &o)
                    {
                        return &(o.get()) == &observer;
