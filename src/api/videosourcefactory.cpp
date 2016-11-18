c7496cc50e83dac3bc8a5702114559fc49579ddd
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-11-18 15:09:08 +0000

    Issue #121: amended free_device with device check as in Factory::disconnect

--- src/api/videosourcefactory.cpp
+++ src/api/videosourcefactory.cpp
@@ -26,10 +26,33 @@ IVideoSource * VideoSourceFactory::get_device(Device device,
                                               ColourSpace colour)
 {
     // TODO
+    if (_devices[(int) device] == nullptr)
+    {
+        _devices[(int) device] = new DummyVideoSource;
+        _device_colours[(int) device] = colour;
+    }
+    else if (_device_colours[(int) device] != colour)
+        throw DeviceAlreadyConnected(
+                "Device already connected using another colour space"
+        );
+    return _devices[(int) device];
 }
 
 void VideoSourceFactory::free_device(Device device)
 {
+    switch (device)
+    {
+    case DVI2PCIeDuo_DVI:
+    case DVI2PCIeDuo_SDI:
+        break; // everything up to here is recognised
+    default:
+        std::string msg;
+        msg.append("Device ")
+           .append(std::to_string(device))
+           .append(" not recognised");
+        throw DeviceNotFound(msg);
+    }
+
     if (_devices[(int) device] != nullptr)
         delete _devices[(int) device];
     _devices[(int) device] = nullptr;
