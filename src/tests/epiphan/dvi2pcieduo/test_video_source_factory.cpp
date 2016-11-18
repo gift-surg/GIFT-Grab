2239dfa14ee102f9d6b291743d00a3bc603910f9
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-11-18 11:55:29 +0000

    Issue #121: migrated VideoSourceFactory tests from Python py.test suite over to C++ Catch suite (still need to find a way of parametrizing the colour space and device)

--- src/tests/epiphan/dvi2pcieduo/test_video_source_factory.cpp
+++ src/tests/epiphan/dvi2pcieduo/test_video_source_factory.cpp
@@ -2,3 +2,79 @@
 
 #include "videosourcefactory.h"
 #include "catch.hpp"
+
+// TODO Parametrize device and colour
+gg::Device device = gg::DVI2PCIeDuo_DVI;
+gg::ColourSpace colour = gg::I420;
+
+TEST_CASE( "get_instance returns singleton", "[VideoSourceFactory]" )
+{
+    gg::VideoSourceFactory & factory = gg::VideoSourceFactory::get_instance();
+    gg::VideoSourceFactory & factory_ = gg::VideoSourceFactory::get_instance();
+    REQUIRE( &factory == &factory_ );
+}
+
+TEST_CASE( "get_device returns connection", "[VideoSourceFactory]" )
+{
+    gg::VideoSourceFactory & factory =  gg::VideoSourceFactory::get_instance();
+    IVideoSource * source = nullptr;
+    source = factory.get_device(device, colour);
+    REQUIRE_FALSE( source == nullptr );
+    REQUIRE( source->get_colour() == colour );
+
+    gg::VideoFrame frame(colour, false);
+    REQUIRE( frame.cols() == 0 );
+    REQUIRE( frame.rows() == 0 );
+    REQUIRE( source->get_frame(frame) );
+    REQUIRE( frame.cols() > 0 );
+    REQUIRE( frame.rows() > 0 );
+}
+
+TEST_CASE( "get_device returns singleton", "[VideoSourceFactory]" )
+{
+    gg::VideoSourceFactory & factory =  gg::VideoSourceFactory::get_instance();
+    IVideoSource * source = factory.get_device(device, colour);
+    IVideoSource * source_ = factory.get_device(device, colour);
+    REQUIRE_FALSE( source == nullptr );
+    REQUIRE( source == source_ );
+}
+
+TEST_CASE( "get_device does not create duplicate", "[VideoSourceFactory]" )
+{
+    gg::VideoSourceFactory & factory = gg::VideoSourceFactory::get_instance();
+    gg::ColourSpace other_colour;
+    switch(colour)
+    {
+    case gg::I420:
+        other_colour = gg::BGRA;
+        break;
+    case gg::BGRA:
+        other_colour = gg::I420;
+        break;
+    default:
+        FAIL("Colour space unknown");
+        break;
+    }
+    IVideoSource * source = factory.get_device(device, colour);
+    IVideoSource * source_duplicate = nullptr;
+    REQUIRE_THROWS_AS(
+        source_duplicate = factory.get_device(device, other_colour),
+        gg::DeviceAlreadyConnected
+    );
+    REQUIRE( source_duplicate == nullptr );
+}
+
+TEST_CASE( "Factory garbage-collects devices", "[VideoSourceFactory]" )
+{
+    IVideoSource * source_dvi = nullptr;
+    IVideoSource * source_sdi = nullptr;
+    { // artificial scope: factory is deleted at the end
+        gg::VideoSourceFactory & factory = gg::VideoSourceFactory::get_instance();
+        source_dvi = factory.get_device(gg::DVI2PCIeDuo_DVI, colour);
+        REQUIRE_FALSE( source_dvi == nullptr );
+        source_sdi = factory.get_device(gg::DVI2PCIeDuo_SDI, colour);
+        REQUIRE_FALSE( source_sdi == nullptr );
+    }
+    REQUIRE( source_dvi == nullptr );
+    REQUIRE( source_sdi == nullptr );
+}
