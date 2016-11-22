91ddff6e59b1b937a3ebdfe590eb0cc7a22185f8
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-11-22 09:57:15 +0000

    Issue #122: added global frame_rate declaration to frame rate test in targets test_unit

--- src/tests/target/test_unit.py
+++ src/tests/target/test_unit.py
@@ -93,7 +93,7 @@ def test_append_with_colour_mismatch(codec, colour_space):
 
 
 def test_frame_rate(codec):
-    global target, file_name
+    global target, file_name, frame_rate
     for i in range(10):
         target.append(frame)
     target.finalise()
