8c60cdf1f26db5b6a5a74f2d2ab76145783e7e2b
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-11-29 10:08:06 +0000

    Issue #132: revised tests for syntactic and semantic correctness

--- src/tests/videoframe/test_numpy_compatibility.py
+++ src/tests/videoframe/test_numpy_compatibility.py
@@ -1,6 +1,9 @@
 from pytest import mark, yield_fixture, fail
-from pygiftgrab import VideoFrame
+from pygiftgrab import VideoFrame, ColourSpace
+# TODO: what if no NumPy?
+import numpy as np
 
+# TODO: Include value tests
 
 frame = None
 cols = 1920
@@ -13,26 +16,42 @@ def peri_test(colour_space):
     frame = VideoFrame(colour_space, cols, rows)
     assert frame is not None
 
+    yield
+
 
 @mark.numpy_compatibility
-def test_data(colour_space):
+def test_data():
     global frame, cols, rows
-    data_numpy = frame.data()
-    assert data_numpy.data_type == uchar
-    assert data_numpy.size(0) == cols
-    assert data_numpy.size(1) == rows
+    data_np = frame.data()
+    assert data_np.dtype == np.int8
+    assert frame.data_length() == data_np.size
 
 
 @mark.numpy_compatibility
-def test_data_length(colour_space):
+def test_access(colour_space):
     global frame
     global cols, rows
-    data_numpy = frame.data()
-    assert frame.data_length() == data_numpy.total_size()
-    try:
-        data_numpy(0, 0)
-        data_numpy(0, rows - 1)
-        data_numpy(cols - 1, 0)
-        data_numpy(cols - 1, rows - 1)
-    except IndexError as e:
-        fail(e.message)
+    data_np = frame.data()
+
+    # general indices sampled at the boundaries only
+    # intermediate indices should be accessible,
+    # provided that these are accessible
+    indices = [col * rows + row
+               for col in [0, cols - 1]
+               for row in [0, rows - 1]]
+
+    # currently performing no value checks
+    for index in indices:
+        try:
+            if colour_space == ColourSpace.BGRA:
+                data_np[index * 4 + 0]  # Blue channel
+                data_np[index * 4 + 1]  # Green channel
+                data_np[index * 4 + 2]  # Red channel
+                data_np[index * 4 + 3]  # Alpha channel
+            elif colour_space == ColourSpace.I420:
+                # see https://wiki.videolan.org/YUV/#I420
+                data_np[index]         # Y plane
+                data_np[index * 1.25]  # U plane
+                data_np[index * 1.5]   # V plane
+        except IndexError as e:
+            fail(e.message)
