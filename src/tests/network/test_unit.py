20a920724e758fa313d5d444899cca9b9c1e3b33
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-12-05 10:26:49 +0000

    Issue #85: DRYed test_unit of network sources

--- src/tests/network/test_unit.py
+++ src/tests/network/test_unit.py
@@ -5,6 +5,32 @@ from pygiftgrab import VideoSourceFactory, VideoFrame, ColourSpace
 source = None
 frame = None
 frame_with_colour_mismatch = None
+tmp_frame = None
+
+
+def generate_sub_frame(x_factor, y_factor, width_factor, height_factor):
+    # full frame dimensions
+    width, height = get_frame_dimensions()
+
+    # generate sub frame dimensions
+    sub_width = width // width_factor
+    sub_height = height // height_factor
+    sub_x = width // x_factor
+    sub_y = height // y_factor
+
+    # do health check on sub frame dimensions
+    assert sub_x + sub_width < width
+    assert sub_y + sub_height < height
+
+    return sub_x, sub_y, sub_width, sub_height
+
+
+def get_frame_dimensions():
+    global source
+    assert source is not None
+    global tmp_frame
+    assert source.get_frame(tmp_frame)
+    return tmp_frame.cols(), tmp_frame.rows()
 
 
 @yield_fixture(scope='session')
@@ -14,8 +40,9 @@ def peri_test(address, colour_space):
     source = factory.connect_network_source(address, colour_space)
     assert source is not None
 
-    global frame, frame_with_colour_mismatch
+    global frame, frame_with_colour_mismatch, tmp_frame
     frame = VideoFrame(colour_space, False)
+    tmp_frame = VideoFrame(colour_space, False)
     if colour_space == ColourSpace.BGRA:
         mismatch_colour_space = ColourSpace.I420
     else:
@@ -40,36 +67,17 @@ def test_get_frame():
 @mark.usefixtures('peri_test')
 def test_sub_frame():
     global source, frame
-    source.get_frame(frame)
-    width = frame.cols()
-    height = frame.rows()
 
     # a first sub-frame
-    sub_width = width // 3
-    sub_height = height // 3
-    sub_x = width // 2
-    sub_y = height // 2
-    # health checks
-    assert sub_x + sub_width < width
-    assert sub_y + sub_height < height
-
-    source.set_sub_frame(sub_x, sub_y,
-                         sub_width, sub_height)
+    sub_x, sub_y, sub_width, sub_height = generate_sub_frame(2, 2, 3, 3)
+    source.set_sub_frame(sub_x, sub_y, sub_width, sub_height)
     assert source.get_frame(frame)
     assert frame.cols() == sub_width
     assert frame.rows() == sub_height
 
     # a second sub-frame
-    sub2_width = width // 5
-    sub2_height = height // 5
-    sub2_x = width // 6
-    sub2_y = height // 6
-    # health checks
-    assert sub2_x + sub2_width < width
-    assert sub2_y + sub2_height < height
-
-    source.set_sub_frame(sub2_x, sub2_y,
-                         sub2_width, sub2_height)
+    sub2_x, sub2_y, sub2_width, sub2_height = generate_sub_frame(6, 6, 5, 5)
+    source.set_sub_frame(sub2_x, sub2_y, sub2_width, sub2_height)
     assert source.get_frame(frame)
     assert frame.cols() == sub2_width
     assert frame.rows() == sub2_height
