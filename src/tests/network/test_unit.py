705e32136b2516f3403ec49d039a85610d78a153
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-12-05 09:57:33 +0000

    Issue #85: implemented test_get_frame of test_unit for network sources

--- src/tests/network/test_unit.py
+++ src/tests/network/test_unit.py
@@ -1,18 +1,39 @@
-from pytest import yield_fixture, fail, mark
+from pytest import yield_fixture, mark
 from pygiftgrab import VideoSourceFactory, VideoFrame, ColourSpace
+
+
+source = None
+frame = None
+frame_with_colour_mismatch = None
 
 
 @yield_fixture(scope='session')
 def peri_test(address, colour_space):
-    # TODO
+    factory = VideoSourceFactory.get_instance()
+    global source
+    source = factory.connect_network_source(address, colour_space)
+    assert source is not None
+
+    global frame, frame_with_colour_mismatch
+    frame = VideoFrame(colour_space, False)
+    if colour_space == ColourSpace.BGRA:
+        mismatch_colour_space = ColourSpace.I420
+    else:
+        mismatch_colour_space = ColourSpace.BGRA
+    frame_with_colour_mismatch = VideoFrame(mismatch_colour_space, False)
+
     yield
 
 
 @mark.unit
 @mark.usefixtures('peri_test')
 def test_get_frame():
-    # TODO
-    assert False
+    global source, frame
+    assert source.get_frame(frame)
+    assert frame.rows() > 0
+    assert frame.cols() > 0
+
+    assert not source.get_frame(frame_with_colour_mismatch)
 
 
 @mark.unit
