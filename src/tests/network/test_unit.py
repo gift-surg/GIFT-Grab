5e9d45c809e4ca4efc68d0505e3148b5974fb993
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-12-05 10:02:32 +0000

    Issue #85: implemented test_sub_frame of test_unit for network sources

--- src/tests/network/test_unit.py
+++ src/tests/network/test_unit.py
@@ -39,8 +39,40 @@ def test_get_frame():
 @mark.unit
 @mark.usefixtures('peri_test')
 def test_sub_frame():
-    # TODO
-    assert False
+    global source, frame
+    source.get_frame(frame)
+    width = frame.cols()
+    height = frame.rows()
+
+    # a first sub-frame
+    sub_width = width // 3
+    sub_height = height // 3
+    sub_x = width // 2
+    sub_y = height // 2
+    # health checks
+    assert sub_x + sub_width < width
+    assert sub_y + sub_height < height
+
+    source.set_sub_frame(sub_x, sub_y,
+                         sub_width, sub_height)
+    assert source.get_frame(frame)
+    assert frame.cols() == sub_width
+    assert frame.rows() == sub_height
+
+    # a second sub-frame
+    sub2_width = width // 5
+    sub2_height = height // 5
+    sub2_x = width // 6
+    sub2_y = height // 6
+    # health checks
+    assert sub2_x + sub2_width < width
+    assert sub2_y + sub2_height < height
+
+    source.set_sub_frame(sub2_x, sub2_y,
+                         sub2_width, sub2_height)
+    assert source.get_frame(frame)
+    assert frame.cols() == sub2_width
+    assert frame.rows() == sub2_height
 
 
 @mark.unit
