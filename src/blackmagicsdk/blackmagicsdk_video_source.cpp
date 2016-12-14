9c1956725a20e68c7d6b00e308ded18ca68cdad4
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-12-14 09:58:21 +0000

    Issue #5: renamed FrameGrabberBlackMagic => VideoSourceBlackmagicSDK, and removed all of old code: only skeleton remaining now

--- src/blackmagicsdk/blackmagicsdk_video_source.cpp
+++ src/blackmagicsdk/blackmagicsdk_video_source.cpp
@@ -1,126 +1,57 @@
-// This class is based on the work by Luis Carlos Garcia-Peraza Herrera
-#include "blackmagic_frame_grabber.h"
-#include <stdexcept>
+#include "blackmagicsdk_video_source.h"
 
-FrameGrabberBlackMagic::FrameGrabberBlackMagic()
-    :IVideoSource(), _deck_link(0), _deck_link_input(0), _delegate(0), _initialized(false)
-{}
-
-FrameGrabberBlackMagic::FrameGrabberBlackMagic(BMDDisplayMode display_mode, BMDPixelFormat pixel_format)
+namespace gg
 {
-    BMDDisplayModeSupport dm_supported;
-    IDeckLinkIterator *dc_iter = CreateDeckLinkIteratorInstance();
-    if (!dc_iter) {
-        throw std::runtime_error("Failed to create the DeckLink iterator interface. "
-                                 "Please install the DeckLink drivers");
-    }
 
-    HRESULT result = S_OK;
-    bool card_found = false;
-    while ((result = dc_iter->Next(&_deck_link)) == S_OK) {
-        if (_deck_link->QueryInterface(IID_IDeckLinkInput, (void**)&_deck_link_input) == S_OK) {
-            // Check if this supports the required display mode
-            if (_deck_link_input->DoesSupportVideoMode(display_mode, pixel_format, bmdVideoInputFlagDefault,
-                                                       &dm_supported, NULL) == S_OK) {
-                // This is the card we want
-                if (dm_supported == bmdDisplayModeSupported) {
-                    card_found = true;
-                    break;
-                }
-            }
-            else {
-                _deck_link->Release();
-            }
-        }
-        else {
-            _deck_link->Release();
-        }
-    }
-
-    // Release the iterator
-    dc_iter->Release();
-
-    if (result != S_OK || _deck_link == NULL) {
-        throw std::runtime_error("Failed to find a BlackMagic card.");
-    }
-
-    if (!card_found) {
-        throw std::runtime_error("Could not find a card which supports the desired display format.");
-    }
-
-    // not clear if this ever has to be deleted or 'Released'. The BlackMagic driver and API is not great.
-    // However, delete or release seems to crash it. So, do not try and fix this 'bug'.
-    _delegate = new DeckLinkCaptureDelegate();
-
-    if (!_delegate) {
-        throw std::runtime_error("Failed to create decklink delegate");
-    }
-
-    if (_deck_link_input->SetCallback(_delegate) != S_OK) {
-        throw std::runtime_error("Failed to set callback in the DeckLink input");
-    }
-
-    if (_deck_link_input->EnableVideoInput(display_mode, pixel_format, bmdVideoInputFlagDefault) != S_OK) {
-        throw std::runtime_error("Failed to enable video input.");
-    }
-
-    start_capture();
-
-    _initialized = true;
+VideoSourceBlackmagicSDK::VideoSourceBlackmagicSDK()
+    : IVideoSource()
+{
+    // nop
 }
 
-FrameGrabberBlackMagic::~FrameGrabberBlackMagic()
-{
-    stop_capture();
-    if (_delegate) {
 
-    }
+VideoSourceBlackmagicSDK::VideoSourceBlackmagicSDK(ColourSpace colour)
+    : IVideoSource(colour)
+{
+    // TODO
 }
 
-void FrameGrabberBlackMagic::start_capture()
+
+VideoSourceBlackmagicSDK::~VideoSourceBlackmagicSDK()
 {
-    if (!_deck_link_input || _deck_link_input->StartStreams() != S_OK) {
-        throw std::runtime_error("Could not start video capture.");
-    }
+    // TODO
 }
 
-void FrameGrabberBlackMagic::stop_capture()
+
+bool VideoSourceBlackmagicSDK::get_frame_dimensions(int & width, int & height)
 {
-    if (_deck_link_input) {
-        // Ignore any issues here..
-        _deck_link_input->StopStreams();
-    }
+    // TODO
 }
 
-bool FrameGrabberBlackMagic::get_frame_dimensions(int & width, int & height)
-{
-    if (_delegate) {
-        width = _delegate->WIDTH; height = _delegate->HEIGHT;
-        return (width != 0 && height != 0);
-    }
-    else {
-        width = height = 0;
-        return false;
-    }
-}
 
-double FrameGrabberBlackMagic::get_frame_rate()
+bool VideoSourceBlackmagicSDK::get_frame(VideoFrame & frame)
 {
-    return 25.f;
-}
-
-bool FrameGrabberBlackMagic::get_frame(VideoFrame_BGRA &frame)
-{
-    if (_initialized) {
-        cv::Mat mat = _delegate->getFrame();
-        if (_get_sub_frame) {
-            mat(_sub_frame).copyTo(_buffer_sub_bgra);
-            frame.init_from_opencv_mat(_buffer_sub_bgra);
-        }
-        else {
-            frame.init_from_opencv_mat(mat);
-        }
-        return true;
-    }
+    // TODO
     return false;
+}
+
+
+double VideoSourceBlackmagicSDK::get_frame_rate()
+{
+    // TODO
+    return 0;
+}
+
+
+void VideoSourceBlackmagicSDK::set_sub_frame(int x, int y, int width, int height)
+{
+    // TODO
+}
+
+
+void VideoSourceBlackmagicSDK::get_full_frame()
+{
+    // TODO
+}
+
 }
--- src/blackmagicsdk/blackmagicsdk_video_source.h
+++ src/blackmagicsdk/blackmagicsdk_video_source.h
@@ -1,101 +1,54 @@
-#ifndef __BLACKMAGIC_VIDEOSOURCE_H__
-#define __BLACKMAGIC_VIDEOSOURCE_H__
+#pragma once
 
 #include "ivideosource.h"
 #include "macros.h"
-#include "DeckLinkCaptureDelegate.h"
-// OpenCV includes
-#include "opencv2/opencv.hpp"
+
+namespace gg
+{
 
 //!
 //! \brief This class is used for streaming video from BlackMagic
 //! framegrabbers
 //!
-//! \author Based on the work by Luis Carlos Garcia-Peraza Herrera
+//! \author Based on the work by Luis Carlos Garcia-Peraza-Herrera
 //!
-class FrameGrabberBlackMagic: public IVideoSource
+class VideoSourceBlackmagicSDK : public IVideoSource
 {
-public:
+protected:
     //!
     //! \brief Default constructor that initialises nothing
     //!
-    FrameGrabberBlackMagic();
+    VideoSourceBlackmagicSDK();
+
+public:
+    //!
+    //! \brief Initialise communication with a Blackmagic
+    //! framegrabber for capturing frames in the passed \c colour
+    //! space
+    //! \param colour
+    //! \throw VideoSourceError with a detailed error message if
+    //! connection attempt fails
+    //!
+    VideoSourceBlackmagicSDK(ColourSpace colour);
 
     //!
-    //! \brief Initialise communication with framegrabber after
-    //! checking available hardware supports \c display_mode and
-    //! \c pixel_format
-    //! \param display_mode
-    //! \param pixel_format
+    //! \brief Finalise communication with used framegrabber and
+    //! release it
     //!
-    FrameGrabberBlackMagic(BMDDisplayMode display_mode, BMDPixelFormat pixel_format);
+    virtual ~VideoSourceBlackmagicSDK();
 
-    //!
-    //! \brief Finalise communication with used framegrabber
-    //!
-    virtual ~FrameGrabberBlackMagic();
+    bool get_frame_dimensions(int & width, int & height) override;
 
-    bool get_frame_dimensions(int &width, int &height);
+    bool get_frame(VideoFrame & frame) override;
 
-    bool get_frame(VideoFrame_BGRA &frame);
+    double get_frame_rate() override;
 
-    double get_frame_rate();
+    void set_sub_frame(int x, int y, int width, int height) override;
 
-    void set_sub_frame(int x, int y, int width, int height)
-    {
-        _sub_frame = cv::Rect(x, y, width, height);
-        _get_sub_frame = true;
-        _buffer_sub_bgra = cv::Mat::zeros(height, width, CV_8UC4);
-    }
+    void get_full_frame() override;
 
 private:
-    //!
-    //! \brief Start actual streaming
-    //! \throw std::runtime_error if framegrabber not initialised
-    //! properly or if streaming cannot be started
-    //!
-    void start_capture();
-
-    //!
-    //! \brief Stop streaming if framegrabber present and in use
-    //!
-    void stop_capture();
-
-    //!
-    //! \brief Used for querying available hardware
-    //!
-    IDeckLink *                                 _deck_link;
-
-    //!
-    //! \brief Used for initialising found hardware and starting
-    //! streaming
-    //!
-    IDeckLinkInput *                            _deck_link_input;
-
-    //!
-    //! \brief Gets frames
-    //!
-    DeckLinkCaptureDelegate *                   _delegate;
-
-    //!
-    //! \brief BGRA Buffer for frame ROI
-    //!
-    cv::Mat                                     _buffer_sub_bgra;
-
-    //!
-    //! \brief Descriptor for frame ROI
-    //! \sa MaskFrame
-    //!
-    cv::Rect                                    _sub_frame;
-
-    //!
-    //! \brief Flag indicating whether initalisation and streaming
-    //! are OK
-    //!
-    bool                                        _initialized;
-
-    DISALLOW_COPY_AND_ASSIGNMENT(FrameGrabberBlackMagic);
-
+    DISALLOW_COPY_AND_ASSIGNMENT(VideoSourceBlackmagicSDK);
 };
 
-#endif
+}
