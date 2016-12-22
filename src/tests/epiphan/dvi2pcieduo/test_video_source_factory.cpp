f13b7fc585c96d6b4d3b24b5380c5a816255d8cf
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2016-12-22 10:52:46 +0000

    Issue #85: 'free_device garbage-collects device' test case activated only with EpiphanSDK for Epiphan DVI2PCIe Duo support, due to issue #125

--- src/tests/epiphan/dvi2pcieduo/test_video_source_factory.cpp
+++ src/tests/epiphan/dvi2pcieduo/test_video_source_factory.cpp
@@ -89,6 +89,7 @@ TEST_CASE( "get_device does not create duplicate", "[VideoSourceFactory]" )
     REQUIRE( source_duplicate == nullptr );
 }
 
+#ifdef USE_EPIPHANSDK  // due to issue #125
 TEST_CASE( "free_device garbage-collects device", "[VideoSourceFactory]" )
 {
     gg::VideoSourceFactory & factory = gg::VideoSourceFactory::get_instance();
@@ -107,5 +108,7 @@ TEST_CASE( "free_device garbage-collects device", "[VideoSourceFactory]" )
     source = factory.get_device(device, other_colour);
     REQUIRE_FALSE( source == nullptr );
 }
+#endif
+
 #endif
 #endif
