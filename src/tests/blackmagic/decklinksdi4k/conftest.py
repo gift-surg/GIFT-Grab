22c9bcb8e4713f5ea05f343ec598502f1e4d07ed
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-01-13 15:07:37 +0000

    Issue #5: added UYVY colour space as an option to Blackmagic's conftest.py

--- src/tests/blackmagic/decklinksdi4k/conftest.py
+++ src/tests/blackmagic/decklinksdi4k/conftest.py
@@ -4,7 +4,7 @@ from pygiftgrab import ColourSpace
 
 def pytest_addoption(parser):
     parser.addoption('--colour-space', action='store',
-                     help='Colour space specification (BGRA or I420)')
+                     help='Colour space specification (BGRA, I420, or UYVY)')
     parser.addoption('--frame-rate', action='store', type=int,
                      help='Frame-rate specification for test')
     parser.addoption('--observers', action='store', type=int,
@@ -29,6 +29,8 @@ def colour_space(request):
         return ColourSpace.BGRA
     elif case_insensitive == 'i420':
         return ColourSpace.I420
+    elif case_insensitive == 'uyvy':
+        return ColourSpace.UYVY
     else:
         raise RuntimeError('Could not recognise colour space ' +
                            colour_space)
