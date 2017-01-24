f8d125cf5449a15bbb90f2fec107bcf2de536608
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-01-24 13:14:54 +0000

    Issue #74: added test stubs in test_observer.py of files

--- src/tests/files/conftest.py
+++ src/tests/files/conftest.py
@@ -3,10 +3,20 @@ from pygiftgrab import Codec, ColourSpace
 
 
 def pytest_addoption(parser):
-    parser.addoption('--codec', action='store',
+    parser.addoption('--codec', action='store', type=str, required=True,
                      help='Codec used for encoding file (Xvid, HEVC, or VP9)')
-    parser.addoption('--colour-space', action='store',
+    parser.addoption('--colour-space', action='store', type=str, required=True,
                      help='Colour space specification (BGRA or I420)')
+    parser.addoption('--filepath', action='store', type=str, required=True,
+                     help='Video file to use')
+    parser.addoption('--frame-rate', action='store', type=float, required=True,
+                     help='Frame rate of video file')
+    parser.addoption('--frame-count', action='store', type=int, required=True,
+                     help='No. of frames in video file')
+    parser.addoption('--frame-width', action='store', type=int, required=True,
+                     help='Width of frames in video file')
+    parser.addoption('--frame-height', action='store', type=int, required=True,
+                     help='Height of frames in video file')
 
 
 @fixture(scope='session')
@@ -36,3 +46,48 @@ def colour_space(request):
     else:
         raise RuntimeError('Could not recognise colour space ' +
                            colour_space)
+
+
+@fixture(scope='session')
+def filepath(request):
+    filepath = request.config.getoption('--filepath')
+    if not filepath:
+        raise RuntimeError('Filepath cannot be empty')
+    else:
+        return filepath
+
+
+@fixture(scope='session')
+def frame_rate(request):
+    frame_rate = float(request.config.getoption('--frame-rate'))
+    if frame_rate <= 0:
+        raise RuntimeError('Frame rate must be positive')
+    else:
+        return frame_rate
+
+
+@fixture(scope='session')
+def frame_count(request):
+    frame_count = int(request.config.getoption('--frame-count'))
+    if frame_count <= 0:
+        raise RuntimeError('Frame count must be positive')
+    else:
+        return frame_count
+
+
+@fixture(scope='session')
+def frame_width(request):
+    frame_width = int(request.config.getoption('--frame-width'))
+    if frame_width <= 0:
+        raise RuntimeError('Frame width must be positive')
+    else:
+        return frame_width
+
+
+@fixture(scope='session')
+def frame_height(request):
+    frame_height = int(request.config.getoption('--frame-height'))
+    if frame_height <= 0:
+        raise RuntimeError('Frame height must be positive')
+    else:
+        return frame_height
--- src/tests/files/test_observer.py
+++ src/tests/files/test_observer.py
@@ -1,7 +1,40 @@
-from pytest import fail, mark
+from pytest import fail, mark, yield_fixture
+from pygiftgrab import VideoSourceFactory
 
-# TODO
 
-@mark.unit
-def test_dummy(codec, colour_space):
+factory = None
+video_duration = 0  # inferred, in sec
+
+
+@yield_fixture(scope='session')
+def peri_test(codec, colour_space, filepath,
+              frame_rate, frame_count,
+              frame_width, frame_height):
+	factory = VideoSourceFactory.get_instance()
+	video_duration = frame_count / frame_rate
+	yield
+
+
+@mark.observer_pattern
+def test_valid_filepath_returns_raii_reader(
+	filepath, codec, colour_space,
+	frame_rate, frame_count,
+	frame_width, frame_height
+):
+	fail('tests not implemented yet')
+
+
+@mark.observer_pattern
+def test_reader_releases_file_on_destruction(
+	filepath, codec, colour_space,
+	frame_rate, frame_count,
+	frame_width, frame_height
+):
+	fail('tests not implemented yet')
+
+
+@mark.observer_pattern
+def test_invalid_filepath_throws_exception(
+	codec, colour_space
+):
 	fail('tests not implemented yet')
