fc150912ecc634130ee34425aedadb11ff819266
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-02-13 09:25:55 +0000

    Issue #74: fixed tab-based indentation of test_observer.py of files

--- src/tests/files/test_observer.py
+++ src/tests/files/test_observer.py
@@ -18,12 +18,12 @@ quarter_video_duration = 0  # inferred, in sec
 def peri_test(codec, colour_space, filepath,
               frame_rate, frame_count,
               frame_width, frame_height):
-	global factory
-	factory = VideoSourceFactory.get_instance()
-	global video_duration, quarter_video_duration
-	video_duration = frame_count / frame_rate
-	quarter_video_duration = video_duration / 4
-	yield
+    global factory
+    factory = VideoSourceFactory.get_instance()
+    global video_duration, quarter_video_duration
+    video_duration = frame_count / frame_rate
+    quarter_video_duration = video_duration / 4
+    yield
 
 
 @mark.observer_pattern
@@ -33,76 +33,76 @@ def test_valid_filepath_returns_raii_reader(
 	frame_rate, frame_count,
 	frame_width, frame_height
 ):
-	source = None
-	global factory
-	global video_duration, quarter_video_duration
-	source = factory.create_file_reader(
-		filepath, codec, colour_space
-	)
-	assert source is not None
+    source = None
+    global factory
+    global video_duration, quarter_video_duration
+    source = factory.create_file_reader(
+        filepath, codec, colour_space
+    )
+    assert source is not None
 
-	file_checker = FileChecker(source)
-	file_checker.attach()
-	sleep(video_duration)
-	file_checker.detach()
-	assert file_checker.assert_colour(colour_space)
-	assert file_checker.assert_frame_rate(frame_rate)
-	assert file_checker.assert_frame_dimensions(
-		frame_width, frame_height)
-	assert file_checker.assert_data()
-	assert file_checker.assert_frame_data_lengths(
-		colour_space, frame_width, frame_height)
+    file_checker = FileChecker(source)
+    file_checker.attach()
+    sleep(video_duration)
+    file_checker.detach()
+    assert file_checker.assert_colour(colour_space)
+    assert file_checker.assert_frame_rate(frame_rate)
+    assert file_checker.assert_frame_dimensions(
+        frame_width, frame_height)
+    assert file_checker.assert_data()
+    assert file_checker.assert_frame_data_lengths(
+        colour_space, frame_width, frame_height)
 
 
 @mark.observer_pattern
 @mark.usefixtures('peri_test')
 def test_reader_releases_file_on_destruction(
-	filepath, codec, colour_space,
-	frame_rate, frame_count,
-	frame_width, frame_height
+    filepath, codec, colour_space,
+    frame_rate, frame_count,
+    frame_width, frame_height
 ):
-	source = None
-	global factory
-	global video_duration, quarter_video_duration
-	source = factory.create_file_reader(
-		filepath, codec, colour_space
-	)
-	assert source is not None
-	file_checker_1 = FileChecker(source)
-	file_checker_1.attach()
-	sleep(quarter_video_duration)
-	file_checker_1.detach()
-	assert file_checker_1.assert_data()
-	del file_checker_1
-	del source
+    source = None
+    global factory
+    global video_duration, quarter_video_duration
+    source = factory.create_file_reader(
+        filepath, codec, colour_space
+    )
+    assert source is not None
+    file_checker_1 = FileChecker(source)
+    file_checker_1.attach()
+    sleep(quarter_video_duration)
+    file_checker_1.detach()
+    assert file_checker_1.assert_data()
+    del file_checker_1
+    del source
 
-	source = None
-	source = factory.create_file_reader(
-		filepath, codec, colour_space
-	)
-	file_checker_2 = FileChecker(source)
-	file_checker_2.attach()
-	sleep(video_duration)
-	file_checker_2.detach()
-	assert file_checker_2.assert_colour(colour_space)
-	assert file_checker_2.assert_frame_rate(frame_rate)
-	assert file_checker_2.assert_frame_dimensions(
-		frame_width, frame_height)
-	assert file_checker_2.assert_data()
-	assert file_checker_2.assert_frame_data_lengths(
-		colour_space, frame_width, frame_height)
+    source = None
+    source = factory.create_file_reader(
+        filepath, codec, colour_space
+    )
+    file_checker_2 = FileChecker(source)
+    file_checker_2.attach()
+    sleep(video_duration)
+    file_checker_2.detach()
+    assert file_checker_2.assert_colour(colour_space)
+    assert file_checker_2.assert_frame_rate(frame_rate)
+    assert file_checker_2.assert_frame_dimensions(
+        frame_width, frame_height)
+    assert file_checker_2.assert_data()
+    assert file_checker_2.assert_frame_data_lengths(
+        colour_space, frame_width, frame_height)
 
 
 @mark.observer_pattern
 @mark.usefixtures('peri_test')
 def test_invalid_filepath_throws_exception(
-	codec, colour_space
+    codec, colour_space
 ):
-	source = None
-	global factory
-	with raises(RuntimeError):
-		source = factory.create_file_reader(
+    source = None
+    global factory
+    with raises(RuntimeError):
+        source = factory.create_file_reader(
             '/this/path/should/never/exist.video',
             codec, colour_space
         )
-	assert source is None
+    assert source is None
