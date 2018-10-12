7c4284a831c9afbb9b5f5d39611f2b584ff3f3c3
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-12 15:37:59 +0100

    Issue #5: added a Python script for testing 3D capture support

---
+++ src/tests/blackmagic/decklink4kextreme12g/bm3d.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python2
+
+import time
+import os
+import cv2
+import numpy as np
+from pygiftgrab import IObservableObserver
+from pygiftgrab import VideoSourceFactory
+from pygiftgrab import ColourSpace
+from pygiftgrab import Device
+from pygiftgrab import VideoFrame
+
+
+class FrameSaver(IObservableObserver):
+
+    def __init__(self, num_to_save):
+        super(FrameSaver, self).__init__()
+        self.num_saved = 0
+        self.num_to_save = num_to_save
+        self.frames = []
+        self.frame_dims = None
+
+    def update(self, frame):
+        if self.num_saved < self.num_to_save:
+            if self.frame_dims is None:
+                self.frame_dims = [frame.rows(), frame.cols()]
+            frame_copy = VideoFrame(
+                ColourSpace.UYVY, frame.rows(), frame.cols()
+            )
+            frame_copy.data(False)[:] = frame.data(False)[:]
+            self.frames.append(frame_copy)
+            self.num_saved += 1
+
+    def dump(self):
+        print('Acquired {} frames'.format(self.num_saved))
+        if self.num_saved < self.num_to_save:
+            print('Still acquiring')
+            return
+        out_folder = os.path.join('.', time.strftime('%Y-%m-%d-%H-%M-%S'))
+        os.mkdir(out_folder)
+        data_bgra = np.zeros(self.frame_dims + [4], np.uint8)
+        for i, frame in enumerate(self.frames):
+            data = frame.data(False)
+            data = np.reshape(data, self.frame_dims + [2])
+            out_file = os.path.join(out_folder, 'frame-{:03d}.png'.format(i+1))
+
+            cv2.cvtColor(src=data, code=cv2.COLOR_YUV2BGRA_UYVY, dst=data_bgra)
+            cv2.imwrite(out_file, data_bgra)
+        print('Saved {} frames in {}'.format(len(self.frames), out_folder))
+
+
+if __name__ == '__main__':
+    sfac = VideoSourceFactory.get_instance()
+    bm = sfac.get_device(Device.DeckLinkSDI4K, ColourSpace.UYVY)
+    frame = VideoFrame(ColourSpace.UYVY, False)
+
+    saver = FrameSaver(10)
+
+    bm.attach(saver)
+
+    time.sleep(2)  # operate pipeline for 20 sec
+
+    bm.detach(saver)
+
+    saver.dump()
