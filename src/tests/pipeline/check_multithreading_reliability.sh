c13a24f7be4c5ccd34f8fe1ded260afffb1e6e25
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-13 08:28:50 +0100

    Issue #16: renamed variable for clarity in run script

--- src/tests/pipeline/check_multithreading_reliability.sh
+++ src/tests/pipeline/check_multithreading_reliability.sh
@@ -6,15 +6,15 @@
 # and NumPy support. Subsequently run the multi-threading
 # reliability check script.
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-BUILD_DIR=$CURRENT_DIR/mtr-build
-SOURCE_DIR="$( cd "$CURRENT_DIR/../.." >/dev/null && pwd )"
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+BUILD_DIR=$ROOT_DIR/mtr-build
+SOURCE_DIR="$( cd "$ROOT_DIR/../.." >/dev/null && pwd )"
 CMAKE_OPTS="-D USE_FILES=ON"
 CMAKE_OPTS="$CMAKE_OPTS -D USE_HEVC=ON"
 CMAKE_OPTS="$CMAKE_OPTS -D ENABLE_NONFREE=ON -D USE_NVENC=ON"
 CMAKE_OPTS="$CMAKE_OPTS -D BUILD_PYTHON=ON -D USE_NUMPY=ON"
 CMAKE_OPTS="$CMAKE_OPTS -D CMAKE_BUILD_TYPE=Debug"
-MTR_SCRIPT=$CURRENT_DIR/multithreading_reliability_check.py
+MTR_SCRIPT=$ROOT_DIR/multithreading_reliability_check.py
 
 mkdir -p $BUILD_DIR
 rm -rf $BUILD_DIR/*
