382c77a257b1084b31da4a7423c1d1759fe2044f
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-13 09:02:44 +0100

    Issue #16: saving all output in a hierarchical session directory

--- src/tests/pipeline/check_multithreading_reliability.sh
+++ src/tests/pipeline/check_multithreading_reliability.sh
@@ -15,13 +15,26 @@ CMAKE_OPTS="$CMAKE_OPTS -D ENABLE_NONFREE=ON -D USE_NVENC=ON"
 CMAKE_OPTS="$CMAKE_OPTS -D BUILD_PYTHON=ON -D USE_NUMPY=ON"
 CMAKE_OPTS="$CMAKE_OPTS -D CMAKE_BUILD_TYPE=Debug"
 MTR_SCRIPT=$ROOT_DIR/multithreading_reliability_check.py
+SESSION_DIR=$ROOT_DIR/$(date +"%Y-%m-%d-%H-%M-%S")
+mkdir $SESSION_DIR
 ulimit -c unlimited
 
+BUILD_LOG=$SESSION_DIR/build.log
+{
 mkdir -p $BUILD_DIR
 rm -rf $BUILD_DIR/*
 cd $BUILD_DIR
 cmake $CMAKE_OPTS $SOURCE_DIR
 make -j4
+} > $BUILD_LOG 2>&1
+
+run_no=1
+WORKING_DIR=$SESSION_DIR/$run_no
+mkdir $WORKING_DIR
+cd $WORKING_DIR
+RUN_LOG=$WORKING_DIR/run.log
+{
 PYTHONPATH=$BUILD_DIR python $MTR_SCRIPT --input $1
+} > $RUN_LOG 2>&1
 
 ulimit -c 0
