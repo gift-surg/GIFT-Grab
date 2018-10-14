d1b7ece3d84597afd51688a13e8ae90674604484
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-14 10:20:01 +0100

    Issue #16: commented checker script

--- src/tests/pipeline/multithreading_reliability_check.py
+++ src/tests/pipeline/multithreading_reliability_check.py
@@ -25,7 +25,11 @@ problem is fixed.
 """
 
 
+# global NumPy buffers
 np_buffer_red, np_buffer_orig = None, None
+
+# mutex protecting frame data passed from node to node
+# in the GIFT-Grab processing pipeline
 lock = threading.Lock()
 
 
@@ -58,22 +62,41 @@ class SnapshotSaver(IObserver):
 
 
 class Bufferer(IObservableObserver):
+    """GIFT-Grab processing node that updates a frame buffer."""
 
     def __init__(self, np_buffer):
+        """Initialise a bufferer that will update given buffer."""
         super(Bufferer, self).__init__()
         self.buffer = np_buffer
 
     def update(self, frame):
+        """Implement ``IObservableObserver.update``."""
         with lock:
             data = frame.data(True)
             self.buffer[:, :, :] = data[:, :, :]
 
 
 class Histogrammer(threading.Thread):
+    """
+    GIFT-Grab processing node that computes the histogram of
+    an image channel and prints how "colored" that channel is.
+    """
 
     channels = ('Blue', 'Green', 'Red', 'Alpha')
 
     def __init__(self, np_buffer, channel, tag, frame_rate, display_freq):
+        """
+        :param np_buffer: image buffer to use
+        :param channel: image channel to compute coloredness for
+        :param tag: a tag describing what this image is, e.g.
+        how it's been processed within a GIFT-Grab pipeline
+        :param frame_rate: the rate at which to compute the
+        coloredness (unit: frames-per-second)
+        :param display_freq: how many times to skip the display
+        of computed coloredness, e.g. if 5 is provided, the
+        coloredness of every 5th frame will be printed to the
+        console
+        """
         super(Histogrammer, self).__init__()
         assert channel in range(3)
         assert 0 < frame_rate <= 60
@@ -87,6 +110,7 @@ class Histogrammer(threading.Thread):
         self.running = False
 
     def run(self):
+        """Override ``Thread.run``."""
         if self.running:
             return
 
@@ -113,12 +137,18 @@ class Histogrammer(threading.Thread):
             sleep(self.sleep_interval)
 
     def stop(self):
+        """Stop the thread."""
         self.running = False
 
 
 class Dyer(IObservableObserver):
+    """Dyes specified channel of an incoming video frame."""
 
     def __init__(self, channel, increment):
+        """
+        :param channel: image channel to dye
+        :param increment: how much to dye the image channel
+        """
         super(Dyer, self).__init__()
         assert 0 <= channel < 3
         assert 0 <= increment < 256
@@ -126,6 +156,7 @@ class Dyer(IObservableObserver):
         self.increment = increment
 
     def update(self, frame):
+        """Implement ``IObservableObserver.update``."""
         with lock:
             data = frame.data(True)
             channel_data = data[:, :, self.channel]
@@ -145,35 +176,46 @@ if __name__ == '__main__':
     assert filename
     assert ext == '.mp4'
 
+    # initialise reading of passed file
     sfac = VideoSourceFactory.get_instance()
     reader = sfac.create_file_reader(in_file, ColourSpace.BGRA)
     frame = VideoFrame(ColourSpace.BGRA, False)
     reader.get_frame(frame)
     frame_shape = (frame.rows(), frame.cols(), 4)
 
+    # prepare for creating encoders (writers)
     tfac = VideoTargetFactory.get_instance()
     frame_rate = reader.get_frame_rate()
 
+    # create a red and green Dyer
     red_dyer = Dyer(2, 128)
     green_dyer = Dyer(1, 64)
 
+    # create the bufferer for the red and green Dyers
     np_buffer_red = np.zeros(frame_shape, np.uint8)
     bufferer_red = Bufferer(np_buffer_red)
     np_buffer_orig = np.zeros_like(np_buffer_red)
     bufferer_orig = Bufferer(np_buffer_orig)
 
+    # create the histogrammers for the red-dyed and
+    # the original video frames, and start them
     hist_red = Histogrammer(np_buffer_red, 2, 'red-dyed', 60.0, 10)
     hist_red.start()
     hist_orig = Histogrammer(np_buffer_orig, 2, 'original', 50.0, 10)
     hist_orig.start()
 
+    # create encoders for the red-dyed and yellow-dyed (as green
+    # is applied on top of red) video streams
     red_file = os.path.join('.', ''.join([filename, '-red', ext]))
     red_writer = tfac.create_file_writer(Codec.HEVC, red_file, frame_rate)
     yellow_file = os.path.join('.', ''.join([filename, '-yellow', ext]))
     yellow_writer = tfac.create_file_writer(Codec.HEVC, yellow_file, frame_rate)
 
+    # create a SnapshotSavers for saving a number of yellow-dyed
+    # video frames
     yellow_snapshots = SnapshotSaver('.')
 
+    # assemble the GIFT-Grab pipeline
     reader.attach(bufferer_orig)
     bufferer_orig.attach(red_dyer)
     red_dyer.attach(red_writer)
@@ -183,9 +225,12 @@ if __name__ == '__main__':
     green_dyer.attach(yellow_snapshots)
 
     sleep(20)  # operate pipeline for 20 sec
+
+    # stop the histogrammers
     hist_red.stop()
     hist_orig.stop()
 
+    # disassemble the GIFT-Grab pipeline
     reader.detach(bufferer_orig)
     bufferer_orig.detach(red_dyer)
     red_dyer.detach(red_writer)
