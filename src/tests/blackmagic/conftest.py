4546865ea962fa94af376fbad244ac68807ff3db
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-19 08:04:45 +0100

    Issue #23: Blackmagic tests now expect device to be specified as a CLI arg

--- src/tests/blackmagic/conftest.py
+++ src/tests/blackmagic/conftest.py
@@ -1,14 +1,28 @@
 from pytest import fixture
-from pygiftgrab import ColourSpace
+from pygiftgrab import (ColourSpace, Device)
 
 
 def pytest_addoption(parser):
+    parser.addoption('--device', action='store',
+                     help='Device to test (decklinksdi4k, or decklink4kextreme12g)')
     parser.addoption('--colour-space', action='store',
                      help='Colour space specification (BGRA, I420, or UYVY)')
     parser.addoption('--frame-rate', action='store', type=int,
                      help='Frame-rate specification for test')
     parser.addoption('--observers', action='store', type=int,
                      help='Number of observers to use when testing frame rate')
+
+
+@fixture(scope='session')
+def device(request):
+    device = request.config.getoption('--device')
+    case_insensitive = device.lower()
+    if case_insensitive == 'decklinksdi4k':
+        return Device.DeckLinkSDI4K
+    elif case_insensitive == 'decklink4kextreme12g'
+        return Device.DeckLink4KExtreme12G
+    else:
+        raise RuntimeError('Could not recognise device {}'.format(device))
 
 
 @fixture(scope='session')
