7e355c7dd641ddde91de15ce0f8030b901ff5082
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-22 08:48:14 +0100

    Issue #16: using the SDI port of Epiphan DVI2PCIe Duo instead of the DVI port in checker

--- src/tests/pipeline/complex_pipeline.py
+++ src/tests/pipeline/complex_pipeline.py
@@ -160,7 +160,7 @@ if __name__ == '__main__':
     parser.add_argument('-i', '--input', type=str, required=True,
                         metavar='VIDEO_INPUT',
                         help='decklink (for grabbing frames from a Blackmagic DeckLink 4K Extreme 12G),\n'
-                             'dvi2pcie (for grabbing frames from an Epiphan DVI2PCIe Duo DVI port\n'
+                             'dvi2pcie (for grabbing frames from an Epiphan DVI2PCIe Duo SDI port\n'
                              ' or a video file (HEVC-encoded MP4)')
     args = parser.parse_args()
     video_input = args.input
@@ -175,8 +175,8 @@ if __name__ == '__main__':
     elif video_input == 'dvi2pcie':
         filename, ext = 'dvi2pcie', '.mp4'
 
-        # start acquirigin frames from an Epiphan DVI2PCIe Duo DVI port
-        source = sfac.get_device(Device.DVI2PCIeDuo_DVI, ColourSpace.BGRA)
+        # start acquirigin frames from an Epiphan DVI2PCIe Duo SDI port
+        source = sfac.get_device(Device.DVI2PCIeDuo_SDI, ColourSpace.BGRA)
     else:
         filename = os.path.basename(video_input)
         filename, ext = os.path.splitext(filename)
