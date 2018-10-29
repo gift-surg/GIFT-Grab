468473f6b81735190653f25b63e64d0f875ff944
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-29 08:08:56 +0000

    Issue #14: leak checker runner script now allows for core dumps and also does not print OK any more (that part now delegated to app)

--- src/tests/videosourcefactory/repeat-leak-checker.sh
+++ src/tests/videosourcefactory/repeat-leak-checker.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
+
+ulimit -c unlimited
+
 device=$1
 colour=$2
 num_reps=$3
 for i in `seq 1 $num_reps`; do
     printf "${i}. run: "
     ./tests/videosourcefactory/video_source_factory_leak_checker $device $colour
-    printf "OK\n"
     sleep 5
 done
+
+ulimit -c 0
