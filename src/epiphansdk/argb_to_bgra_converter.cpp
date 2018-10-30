530a1a6cdd8910f1af48ebf44fd8f6ad7a6fd0df
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-30 09:04:41 +0000

    Issue #46: added an ArgbToBgraConverter class stub

---
+++ src/epiphansdk/argb_to_bgra_converter.cpp
@@ -0,0 +1,6 @@
+#include "argb_to_bgra_converter.h"
+
+namespace gg
+{
+
+}
---
+++ src/epiphansdk/argb_to_bgra_converter.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace gg
+{
+
+//!
+//! \brief Epiphan SDK seems to support ARGB but not BGRA.
+//! This class abstracts the conversion of captured ARGB
+//! data to BGRA.
+//! \sa VideoSourceEpiphanSDK
+//!
+class ArgbToBgraConverter
+{
+
+};
+
+}
