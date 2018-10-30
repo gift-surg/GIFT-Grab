859d5a9975372aa0e693386dd682165c0636e3ac
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-30 09:15:04 +0000

    Issue #46: added basic constructor and method stubs to ArgbToBgraConverter

--- src/epiphansdk/argb_to_bgra_converter.cpp
+++ src/epiphansdk/argb_to_bgra_converter.cpp
@@ -3,4 +3,26 @@
 namespace gg
 {
 
+ArgbToBgraConverter::ArgbToBgraConverter()
+{
+
+}
+
+ArgbToBgraConverter::~ArgbToBgraConverter()
+{
+
+}
+
+void ArgbToBgraConverter::convert(const unsigned char *argb,
+                                  unsigned char *bgra)
+{
+
+}
+
+void ArgbToBgraConverter::set_frame_dimensions(size_t width,
+                                               size_t height)
+{
+
+}
+
 }
--- src/epiphansdk/argb_to_bgra_converter.h
+++ src/epiphansdk/argb_to_bgra_converter.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdlib>
+
 namespace gg
 {
 
@@ -11,7 +13,39 @@ namespace gg
 //!
 class ArgbToBgraConverter
 {
+public:
+    //!
+    //! \brief Create a converter, which should be
+    //! initialised by informing it of the frame
+    //! dimensions
+    //! \sa set_frame_dimensions
+    //!
+    ArgbToBgraConverter();
 
+    //!
+    //! \brief Free all allocated resources
+    //!
+    ~ArgbToBgraConverter();
+
+public:
+    //!
+    //! \brief Convert ARGB data in passed buffer to BGRA
+    //! data, saving it into the passed output buffer.
+    //! \param argb should be in line with the frame
+    //! dimensions set before
+    //! \param bgra should be at least as large as the
+    //! input buffer
+    //! \sa set_frame_dimensions
+    //!
+    void convert(const unsigned char *argb, unsigned char *bgra);
+
+    //!
+    //! \brief Allocate all resources needed for ARGB to
+    //! BGRA conversion based on passed frame dimensions
+    //! \param width
+    //! \param height
+    //!
+    void set_frame_dimensions(size_t width, size_t height);
 };
 
 }
