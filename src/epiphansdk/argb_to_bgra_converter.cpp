ddd4487d55a3ed06ce70ee759553c9ca4dd3fc63
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-10-30 09:19:08 +0000

    Issue #46: added width and height properties to ArgbToBgraConverter

--- src/epiphansdk/argb_to_bgra_converter.cpp
+++ src/epiphansdk/argb_to_bgra_converter.cpp
@@ -1,9 +1,12 @@
 #include "argb_to_bgra_converter.h"
+#include <assert.h>
 
 namespace gg
 {
 
 ArgbToBgraConverter::ArgbToBgraConverter()
+    : _width(0)
+    , _height(0)
 {
 
 }
@@ -22,7 +25,10 @@ void ArgbToBgraConverter::convert(const unsigned char *argb,
 void ArgbToBgraConverter::set_frame_dimensions(size_t width,
                                                size_t height)
 {
-
+    assert(width > 0);
+    assert(height > 0);
+    _width = width;
+    _height = height;
 }
 
 }
