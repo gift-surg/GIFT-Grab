8772c4aaf5722db969f7460dc49cd1c2a97588cb
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-11-15 07:46:49 +0000

    Issue #18: added unit test stubs for stereo frames

---
+++ src/tests/videoframe/test_stereo.py
@@ -0,0 +1,21 @@
+from pytest import mark
+
+
+@mark.stereo_frames
+def test_valid_index_returns_data_length():
+    raise NotImplementedError
+
+
+@mark.stereo_frames
+def test_invalid_data_index_raises():
+    raise NotImplementedError
+
+
+@mark.stereo_frames
+def test_invalid_data_length_index_raises():
+    raise NotImplementedError
+
+
+@mark.stereo_frames
+def test_stereo_frame_constructor():
+    raise NotImplementedError
