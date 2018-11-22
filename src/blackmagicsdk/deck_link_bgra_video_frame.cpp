1733cf5159ea800bcd9bfa3e1af62429de2ba66f
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2018-11-22 17:47:56 +0000

    Issue #30: added an internal BGRA frame class stub

---
+++ src/blackmagicsdk/deck_link_bgra_video_frame.cpp
@@ -0,0 +1,87 @@
+#include "deck_link_bgra_video_frame.h"
+
+namespace gg
+{
+
+DeckLinkBGRAVideoFrame::DeckLinkBGRAVideoFrame(
+    size_t width, size_t height,
+    LPVOID pixel_buffer, BMDFrameFlags frame_flags)
+{
+    // TODO
+}
+
+DeckLinkBGRAVideoFrame::~DeckLinkBGRAVideoFrame()
+{
+    // TODO
+}
+
+long STDMETHODCALLTYPE
+DeckLinkBGRAVideoFrame::GetWidth(void)
+{
+    // TODO
+}
+
+long STDMETHODCALLTYPE
+DeckLinkBGRAVideoFrame::GetHeight(void)
+{
+    // TODO
+}
+
+long STDMETHODCALLTYPE
+DeckLinkBGRAVideoFrame::GetRowBytes(void)
+{
+    // TODO
+}
+
+virtual HRESULT STDMETHODCALLTYPE
+DeckLinkBGRAVideoFrame::GetBytes(void **buffer)
+{
+    // TODO
+}
+
+BMDFrameFlags STDMETHODCALLTYPE
+DeckLinkBGRAVideoFrame::GetFlags(void)
+{
+    // TODO
+}
+
+BMDPixelFormat STDMETHODCALLTYPE
+DeckLinkBGRAVideoFrame::GetPixelFormat(void)
+{
+    // TODO
+}
+
+HRESULT STDMETHODCALLTYPE
+DeckLinkBGRAVideoFrame::QueryInterface(
+    REFIID iid, LPVOID *ppv
+)
+{
+    // TODO
+}
+
+ULONG STDMETHODCALLTYPE DeckLinkBGRAVideoFrame::AddRef()
+{
+    // TODO
+}
+
+ULONG STDMETHODCALLTYPE DeckLinkBGRAVideoFrame::Release()
+{
+    // TODO
+}
+
+HRESULT STDMETHODCALLTYPE
+DeckLinkBGRAVideoFrame::GetAncillaryData(
+    IDeckLinkVideoFrameAncillary **ancillary
+)
+{
+    // TODO
+}
+
+HRESULT STDMETHODCALLTYPE DeckLinkBGRAVideoFrame::GetTimecode(
+        BMDTimecodeFormat format, IDeckLinkTimecode** timecode
+)
+{
+    // TODO
+}
+
+}
---
+++ src/blackmagicsdk/deck_link_bgra_video_frame.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "DeckLinkAPI.h"
+
+namespace gg
+{
+
+//!
+//! \brief A very un-intelligent implementation of
+//! Blackmagic SDK's video frame interface. This class is
+//! not for external use, as such it does absolutely NO
+//! sanity checking, or management of data passed to it.
+//! It expects ALL of that to be done by the caller.
+//!
+class DeckLinkBGRAVideoFrame : public IDeckLinkVideoFrame
+{
+protected:
+    // members for implementing promised API
+    long            _width;
+    long            _height;
+    BMDFrameFlags   _flags;
+    LPVOID          _pixel_buffer = nullptr;
+    ULONG           _ref_count;
+
+public:
+    //!
+    //! \brief Wrap passed parameters into a Blackmagic
+    //! SDK video frame, with absolutely NO sanity
+    //! checking
+    //! \param width
+    //! \param height
+    //! \param pixel_buffer
+    //! \param frame_flags
+    //!
+    DeckLinkBGRAVideoFrame(size_t width, size_t height,
+                           LPVOID pixel_buffer, BMDFrameFlags frame_flags);
+
+    //!
+    //! \brief Simply destroy this object, DO NOTHING
+    //! about data
+    //!
+    virtual ~DeckLinkBGRAVideoFrame();
+
+public:
+    // inherited methods
+    virtual long STDMETHODCALLTYPE GetWidth(void);
+    virtual long STDMETHODCALLTYPE GetHeight(void);
+    virtual long STDMETHODCALLTYPE GetRowBytes(void);
+    virtual HRESULT STDMETHODCALLTYPE GetBytes(void** buffer);
+    virtual BMDFrameFlags STDMETHODCALLTYPE GetFlags(void);
+    virtual BMDPixelFormat STDMETHODCALLTYPE GetPixelFormat(void);
+
+    virtual HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, LPVOID *ppv);
+    virtual ULONG STDMETHODCALLTYPE AddRef();
+    virtual ULONG STDMETHODCALLTYPE Release();
+
+    // Dummy implementations of remaining methods
+    virtual HRESULT STDMETHODCALLTYPE GetAncillaryData(IDeckLinkVideoFrameAncillary** ancillary);
+    virtual HRESULT STDMETHODCALLTYPE GetTimecode(BMDTimecodeFormat format, IDeckLinkTimecode** timecode);
+};
+
+}
