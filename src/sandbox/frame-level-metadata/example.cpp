85fcc2bcdc0f7c5941b9c091c1d6dd2d0adc88ed
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-03-26 09:52:17 +0000

    Issue #66: painting frames rather than completely dummy

--- src/sandbox/frame-level-metadata/example.cpp
+++ src/sandbox/frame-level-metadata/example.cpp
@@ -8,6 +8,8 @@
 using namespace gg;
 using namespace std;
 
+void paint(unsigned char *buffer, size_t length, size_t width, size_t height);
+
 int main()
 {
     // initialise a frame with dummy data
@@ -17,12 +19,12 @@ int main()
     size_t data_length = VideoFrame::required_data_length(colour, width, height);
     unsigned char *in_data = new unsigned char[data_length],
                   *out_data = new unsigned char[data_length];
-    memset(in_data, 0, data_length);
+    paint(in_data, data_length, width, height);
     in_frame.init_from_specs(in_data, data_length, width, height);
 
     // encode a number of this dummy frame out to file
     float frame_rate = 30.0;
-    size_t num_frames = 10;
+    size_t num_frames = 120;
     IVideoTarget *writer = VideoTargetFactory::get_instance().create_file_writer(HEVC, "out.mp4", frame_rate);
     for (size_t i = 0; i < num_frames; i++)
         writer->update(in_frame);
@@ -33,4 +35,16 @@ int main()
     delete []out_data;
 
     return EXIT_SUCCESS;
+}
+
+void paint(unsigned char *buffer, size_t length, size_t width, size_t height)
+{
+    memset(buffer, 255, length);
+    for (size_t i = 0; i < width; i++)
+        for (size_t j = 0; j < height; j++)
+            for (size_t c = 0; c < 3; c++)
+            {
+                int here = j*4*width+i*4;
+                buffer[here] = here % 256;
+            }
 }
