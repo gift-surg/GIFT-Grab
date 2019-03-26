d447a9eab80858857b9a2569143c44ab816258cf
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-03-26 09:55:03 +0000

    Issue #66: funny shifting frames

--- src/sandbox/frame-level-metadata/example.cpp
+++ src/sandbox/frame-level-metadata/example.cpp
@@ -8,7 +8,9 @@
 using namespace gg;
 using namespace std;
 
-void paint(unsigned char *buffer, size_t length, size_t width, size_t height);
+void paint(unsigned char *buffer, size_t length,
+           size_t width, size_t height,
+           size_t frame_no = 0);
 
 int main()
 {
@@ -19,7 +21,6 @@ int main()
     size_t data_length = VideoFrame::required_data_length(colour, width, height);
     unsigned char *in_data = new unsigned char[data_length],
                   *out_data = new unsigned char[data_length];
-    paint(in_data, data_length, width, height);
     in_frame.init_from_specs(in_data, data_length, width, height);
 
     // encode a number of this dummy frame out to file
@@ -27,7 +28,10 @@ int main()
     size_t num_frames = 120;
     IVideoTarget *writer = VideoTargetFactory::get_instance().create_file_writer(HEVC, "out.mp4", frame_rate);
     for (size_t i = 0; i < num_frames; i++)
+    {
+        paint(in_data, data_length, width, height, 2*i);
         writer->update(in_frame);
+    }
 
     // done, clean up
     delete writer;
@@ -37,7 +41,9 @@ int main()
     return EXIT_SUCCESS;
 }
 
-void paint(unsigned char *buffer, size_t length, size_t width, size_t height)
+void paint(unsigned char *buffer, size_t length,
+           size_t width, size_t height,
+           size_t frame_no)
 {
     memset(buffer, 255, length);
     for (size_t i = 0; i < width; i++)
@@ -45,6 +51,6 @@ void paint(unsigned char *buffer, size_t length, size_t width, size_t height)
             for (size_t c = 0; c < 3; c++)
             {
                 int here = j*4*width+i*4;
-                buffer[here] = here % 256;
+                buffer[here] = (here + frame_no) % 256;
             }
 }
