7f756e4884e57f8e08aeb0524884f3534e5cb502
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-03-28 09:00:41 +0000

    Add missing colon

--- src/ffmpeg/ffmpeg_utils.cpp
+++ src/ffmpeg/ffmpeg_utils.cpp
@@ -8,7 +8,7 @@ std::string to_string(const AVFrame * frame)
 {
     std::string _frame = "AVFrame {";
 
-    _frame.append(" metadata {");
+    _frame.append(" metadata: {");
     AVDictionaryEntry *tag = NULL;
     while ((tag = av_dict_get(frame->metadata, "", tag, AV_DICT_IGNORE_SUFFIX)))
     {
