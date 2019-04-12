1e08650c0fa78f9049b6c79dd94250c6992e260a
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-04-12 08:08:21 +0100

    Revert "DEBUG ONLY: custom message to ensure project-specific FindBoost.cmake script is used"
    This reverts commit d8754f703d54ca691c363333725577a6d27246db.

--- src/cmake/boost.python/FindBoost.cmake
+++ src/cmake/boost.python/FindBoost.cmake
@@ -1,8 +1,6 @@
 # Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
 # file Copyright.txt or https://cmake.org/licensing for details.
 
-message("GIFT-Grab custom FindBoost.cmake script")
-
 #.rst:
 # FindBoost
 # ---------
