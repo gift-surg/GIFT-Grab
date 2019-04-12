5bafeb3fe11c4d4cabe0195e94459187515ca21d
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-04-12 09:50:17 +0100

    Revert "Including Boost headers first in Python wrapper"
    This reverts commit ee0009dacd286f65bb10f8c1200f9eefcbf9cee8.

--- src/python/wrapper.cpp
+++ src/python/wrapper.cpp
@@ -1,8 +1,3 @@
-#include <boost/python.hpp>
-#include <boost/python/exception_translator.hpp>
-#ifdef USE_NUMPY
-#include <boost/python/numpy.hpp>
-#endif
 #include "videosourcefactory.h"
 #include "videotargetfactory.h"
 #include "except.h"
@@ -24,6 +19,11 @@
 #ifdef USE_FFMPEG
 #include "ffmpeg_video_source.h"
 #include "ffmpeg_video_target.h"
+#endif
+#include <boost/python.hpp>
+#include <boost/python/exception_translator.hpp>
+#ifdef USE_NUMPY
+#include <boost/python/numpy.hpp>
 #endif
 
 using namespace boost::python;
