6ae5e58c1af95c8d14872549686735adcf53664f
Dzhoshkun Ismail Shakir <dzhoshkun.shakir@gmail.com>
2019-04-18 15:35:39 +0100

    Issue #104: implemented C++ Epiphan live test app

---
+++ src/tests/epiphan/dvi2pcieduo/save_n_frames_to_images.cpp
@@ -0,0 +1,73 @@
+#include <cstdlib>
+#include <thread>
+#include <chrono>
+#include <fstream>
+#include <string>
+
+#include "iobservable.h"
+#include "iobserver.h"
+#include "videosourcefactory.h"
+#include "device.h"
+
+
+using namespace gg;
+using namespace std;
+
+
+class BgraFrameSaver : public IObserver
+{
+private:
+    size_t _max_num_to_save;
+    size_t _num_saved;
+
+public:
+    BgraFrameSaver(size_t max_num_frames);
+
+    ~BgraFrameSaver();
+
+public:
+    void update(VideoFrame &frame);
+};
+
+int main()
+{
+    VideoSourceFactory &sfac = VideoSourceFactory::get_instance();
+    IVideoSource *epiphan = sfac.get_device(DVI2PCIeDuo_DVI, BGRA);
+    BgraFrameSaver saver(3);
+
+    epiphan->attach(saver);
+
+    this_thread::sleep_for(std::chrono::seconds(10));  // operate pipeline for this duration
+
+    epiphan->detach(saver);
+
+    return EXIT_SUCCESS;
+}
+
+BgraFrameSaver::BgraFrameSaver(size_t max_num_frames)
+    : _max_num_to_save(max_num_frames)
+    , _num_saved(0)
+{
+    // nop
+}
+
+BgraFrameSaver::~BgraFrameSaver()
+{
+    // nop
+}
+
+void BgraFrameSaver::update(VideoFrame &frame)
+{
+    if (_num_saved >= _max_num_to_save)
+        return;
+
+    unsigned char *data = frame.data(true);
+
+    string filename = "snapshot-";
+    filename.append(to_string(_num_saved));
+    filename.append(".bin");
+    ofstream outfile(filename, ofstream::binary);
+    outfile.write(reinterpret_cast<char *>(data), frame.data_length());
+
+    _num_saved++;
+}
